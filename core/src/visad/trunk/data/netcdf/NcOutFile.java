4c737f12ab0eff531de3f1cbb95cec653856c085
Steve Emmerson <steve@unidata.ucar.edu>
1997-11-13 19:54:04 +0000

    Added more members.

--- core/src/visad/trunk/data/netcdf/NcOutFile.java
+++ core/src/visad/trunk/data/netcdf/NcOutFile.java
@@ -4,6 +4,7 @@ package visad.data.netcdf;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
+import java.util.Vector;
 import ucar.netcdf.ArrayInput;
 import ucar.netcdf.Dimension;
 import ucar.netcdf.DimensionIterator;
@@ -23,10 +24,25 @@ NcOutFile
     extends	NcFile
 {
     /**
+     * Whether or not we're in define mode.
+     */
+    protected boolean		inDefineMode = true;
+
+    /**
      * Whether or not to overwrite a previously existing file with the same
      * name.
      */
     protected final boolean	clobber;
+
+    /**
+     * The dimensions of the netCDF file.
+     */
+    protected final Vector	dims = new Vector(5);
+
+    /**
+     * The variables of the netCDF file.
+     */
+    protected final Vector	vars = new Vector(5);
 
 
     /**
@@ -37,5 +53,25 @@ NcOutFile
     {
 	super(path);
 	this.clobber = clobber;
+    }
+
+
+    /**
+     * Define a netCDF dimension.
+     */
+    public void
+    define(NcDim dim)
+    {
+	dims.addElement(dim);
+    }
+
+
+    /**
+     * Define a netCDF variable.
+     */
+    public void
+    define(NcVar var)
+    {
+	vars.addElement(var);
     }
 }
