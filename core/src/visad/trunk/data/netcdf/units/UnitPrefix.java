1b64a922cb72f64b5d953ac94ff5d4ee5eee6b4c
Steve Emmerson <steve@unidata.ucar.edu>
1998-01-20 23:29:28 +0000

    Used to be just "Prefix".

--- core/src/visad/trunk/data/netcdf/units/UnitPrefix.java
+++ core/src/visad/trunk/data/netcdf/units/UnitPrefix.java
@@ -2,7 +2,7 @@
  * Copyright 1997, University Corporation for Atmospheric Research
  * See file LICENSE for copying and redistribution conditions.
  *
- * $Id: UnitPrefix.java,v 1.1 1997-11-17 19:35:05 steve Exp $
+ * $Id: UnitPrefix.java,v 1.2 1998-01-20 23:29:28 steve Exp $
  */
 
 package visad.data.netcdf.units;
@@ -12,7 +12,7 @@ package visad.data.netcdf.units;
  * Class for representing unit prefixes.
  */
 public class
-Prefix
+UnitPrefix
     implements	java.io.Serializable
 {
     /**
@@ -35,7 +35,7 @@ Prefix
      * @exception IllegalArgumentException	One of the arguments was null.
      */
     public
-    Prefix(String name, double value)
+    UnitPrefix(String name, double value)
     {
 	if (name == null)
 	    throw new IllegalArgumentException("Null prefix name");
