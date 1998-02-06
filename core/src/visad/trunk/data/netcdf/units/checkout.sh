617774b12220c728fc93b0c2c47ba410d604407d
Steve Emmerson <steve@unidata.ucar.edu>
1998-02-06 20:58:35 +0000

    *** empty log message ***

--- core/src/visad/trunk/data/netcdf/units/checkout.sh
+++ core/src/visad/trunk/data/netcdf/units/checkout.sh
@@ -3,7 +3,18 @@
 
 # Make a link to the "rules.make" file in the parent directory.
 #
-if cd units; then
-    rm -f rules.make
-    ln -s ../rules.make .
-fi
+set -x
+case $1 in
+    netcdf)
+	;;
+	if cd netcdf/units; then
+	    rm -f rules.make
+	    ln -s ../rules.make .
+	fi
+    units)
+	if cd units; then
+	    rm -f rules.make
+	    ln -s ../rules.make .
+	fi
+	;;
+esac
