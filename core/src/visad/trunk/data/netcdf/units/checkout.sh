a00939e636d4ea1387cb609c76aecc1ad96c4e7b
Steve Emmerson <steve@unidata.ucar.edu>
1998-02-06 21:51:22 +0000

    *** empty log message ***

--- core/src/visad/trunk/data/netcdf/units/checkout.sh
+++ core/src/visad/trunk/data/netcdf/units/checkout.sh
@@ -4,23 +4,10 @@
 # Make a link to the included make(1) files in the parent directory.
 #
 set -x
-case $1 in
-    netcdf)
-	if cd netcdf/units; then
-	    rm -f rules.make
-	    ln -s ../rules.make .
 
-	    rm -f macros.make
-	    ln -s ../macros.make .
-	fi
-	;;
-    units)
-	if cd units; then
-	    rm -f rules.make
-	    ln -s ../rules.make .
-
-	    rm -f macros.make
-	    ln -s ../macros.make .
-	fi
-	;;
-esac
+for file in macros.make rules.make; do
+    if test -r ../$file; then
+	rm -f $file
+	ln -s ../$file .
+    fi
+done
