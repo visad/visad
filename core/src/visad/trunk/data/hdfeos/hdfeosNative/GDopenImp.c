78bd181dacb8e648801cd8a391b3ea1abbeeca7d
Tom Rink <rink@ssec.wisc.edu>
1998-02-27 15:27:38 +0000

    changed package for native interfaces

---
+++ core/src/visad/trunk/data/hdfeos/hdfeosNative/GDopenImp.c
@@ -0,0 +1,23 @@
+#include <jni.h>
+#include "hdfeosLib.h"
+#include <stdio.h>
+#include "mfhdf.h"
+#include "HdfEosDef.h"
+
+JNIEXPORT jint JNICALL 
+Java_visad_data_hdfeos_hdfeosNative_hdfeosLib_GDopen
+( JNIEnv *env, jobject obj, jstring filename, jint access )  {
+
+  int32 file_id;
+  char *f_name;
+
+     f_name = (char *) (*env)->GetStringUTFChars( env, filename, 0);
+
+     file_id = GDopen( (char *)f_name, (int32)access );
+
+     (*env)->ReleaseStringUTFChars(env, filename, f_name );
+
+
+   return file_id;
+
+  }
