84dedcd84ab7469abb4ba8cc37f73a4ea62f472b
Tom Rink <rink@ssec.wisc.edu>
1998-03-04 14:19:16 +0000

    Simple cache strategy for FlatFields

--- core/src/visad/trunk/data/CacheStrategy.java
+++ core/src/visad/trunk/data/CacheStrategy.java
@@ -1,37 +1,66 @@
+//
+// CacheStrategy.java
+//
+
 /*
- * VisAD system for interactive analysis and visualization of numerical
- * data.  Copyright (C) 1996 - 1998 Bill Hibbard, Curtis Rueden, Tom
- * Rink, Dave Glowacki, and Steve Emmerson.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 1, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License in file NOTICE for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * $Id: CacheStrategy.java,v 1.2 1998-02-23 14:33:09 steve Exp $
- */
+VisAD system for interactive analysis and visualization of numerical
+data.  Copyright (C) 1996 - 1998 Bill Hibbard, Curtis Rueden, Tom
+Rink and Dave Glowacki.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License in file NOTICE for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 package visad.data;
 
-import visad.FlatField;
+import visad.*;
+import java.lang.*;
+import java.rmi.*;
 
-
-public class CacheStrategy
+public class CacheStrategy 
 {
-    public int allocate(FlatField[] adaptedFlatFields, 
-			boolean[] adaptedFlatFieldDirty,
-		        long[] adaptedFlatFieldSizes,
-			long[] adaptedFlatFieldTimes)
-    {
-	return 0;	// placeholder
-    }
+
+   public CacheStrategy()
+   {
+
+   }
+
+   public int allocate( FlatField[] adaptedFlatFields,
+                        boolean[] adaptedFlatFieldDirty,
+                        long[] adaptedFlatFieldSizes,
+                        long[] adaptedFlatFieldTimes )
+   {
+
+      int adaptedFlatFieldIndex = 0;
+      long oldest = adaptedFlatFieldTimes[0];
+
+      for ( int ii = 0; ii < adaptedFlatFields.length; ii++ ) 
+      {
+
+         if ( adaptedFlatFields[ii] == null ) 
+         {
+           adaptedFlatFieldIndex = ii;
+           return adaptedFlatFieldIndex;
+         }
+         else if ( adaptedFlatFieldTimes[ii] < oldest ) 
+         {
+           oldest = adaptedFlatFieldTimes[ii];
+           adaptedFlatFieldIndex = ii;
+         }
+      }
+
+      return adaptedFlatFieldIndex;
+   }
+
 }
