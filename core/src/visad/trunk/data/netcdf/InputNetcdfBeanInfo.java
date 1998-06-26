5904b5d6d3ae115d29511dc13696dcdda3b45d0a
VisAD Developers <visad@unidata.ucar.edu>
1998-06-26 20:34:56 +0000

    Second try.  Uses customizer.

--- core/src/visad/trunk/data/netcdf/InputNetcdfBeanInfo.java
+++ core/src/visad/trunk/data/netcdf/InputNetcdfBeanInfo.java
@@ -3,7 +3,7 @@
  * All Rights Reserved.
  * See file LICENSE for copying and redistribution conditions.
  *
- * $Id: InputNetcdfBeanInfo.java,v 1.1 1998-06-24 20:56:09 visad Exp $
+ * $Id: InputNetcdfBeanInfo.java,v 1.2 1998-06-26 20:34:56 visad Exp $
  */
 
 package visad.data.netcdf;
@@ -23,39 +23,10 @@ InputNetcdfBeanInfo
     public BeanDescriptor
     getBeanDescriptor()
     {
-	BeanDescriptor	bd = new BeanDescriptor(InputNetcdf.class);
+	BeanDescriptor	bd = 
+	    new BeanDescriptor(InputNetcdf.class, InputNetcdfCustomizer.class);
 
 	bd.setName("Input NetCDF File");
 	return bd;
-    }
-
-    public PropertyDescriptor[]
-    getPropertyDescriptors()
-    {
-	Class			beanClass = InputNetcdf.class;
-	PropertyDescriptor[]	pds;
-
-	try
-	{
-	    PropertyDescriptor	pathnameDescriptor =
-		new PropertyDescriptor("pathname", beanClass);
-
-	    // pathnameDescriptor.setBound(true);
-	    pathnameDescriptor.setPropertyEditorClass(
-		InputNetcdfPathnameEditor.class);
-
-	    pds = new PropertyDescriptor[]
-	    {
-		pathnameDescriptor,
-		new PropertyDescriptor("data", beanClass, "getData", null),
-	    };
-	}
-	catch (Exception e)
-	{
-	    System.out.println(e.getMessage());
-	    pds = null;
-	}
-
-	return pds;
     }
 }
