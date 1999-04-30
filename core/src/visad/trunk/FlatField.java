193736b48129fccd90cc26a9099a270b6f6b410b
Bill Hibbard <billh@ssec.wisc.edu>
1999-04-30 13:06:13 +0000

    fix bug

--- core/src/visad/trunk/FlatField.java
+++ core/src/visad/trunk/FlatField.java
@@ -2923,9 +2923,20 @@ public class FlatField extends FieldImpl {
         new SetType(((FunctionType) Type).getFlatRange().getComponent(i));
       sets[i] = new DoubleSet(set_type);
     }
+
+    // WLH 30 April 99
+    MathType range_type = ((FunctionType) Type).getRange();
+    RealTupleType domain_type = ((SetType) set.getType()).getDomain();
+    FunctionType func_type = new FunctionType(domain_type, range_type);
+    FlatField new_field =
+      new FlatField(func_type, set, RangeCoordinateSystem,
+                    RangeCoordinateSystems, sets, RangeUnits);
+/* WLH 30 April 99
     FlatField new_field =
       new FlatField((FunctionType) Type, set, RangeCoordinateSystem,
                     RangeCoordinateSystems, sets, RangeUnits);
+*/
+
     if (isMissing()) return new_field;
 
     float[][] values = Set.doubleToFloat(unpackValues());
