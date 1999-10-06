4f17abd302bf7f5a1438423f8217c821fd4c03b2
Bill Hibbard <billh@ssec.wisc.edu>
1999-10-06 12:12:25 +0000

    add examples/TestStereo.java, written by Nick that I forgot to add earlier

---
+++ core/src/visad/trunk/examples/TestStereo.java
@@ -0,0 +1,113 @@
+/*
+VisAD system for interactive analysis and visualization of numerical
+data.  Copyright (C) 1996 - 1999 Bill Hibbard, Curtis Rueden, Tom
+Rink, Dave Glowacki, Steve Emmerson, Tom Whittaker, Don Murray, and
+Tommy Jasmin.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA
+*/
+
+import java.rmi.RemoteException;
+
+import visad.*;
+import java.awt.*;
+import javax.media.j3d.*;
+
+import visad.java3d.DisplayImplJ3D;
+
+public class TestStereo
+	extends TestSkeleton
+{
+  public TestStereo() { }
+
+  public TestStereo(String args[])
+	throws VisADException, RemoteException
+  {
+    super(args);
+  }
+
+  DisplayImpl[] setupData()
+	throws VisADException, RemoteException
+  {
+    RealType vis_radiance = new RealType("vis_radiance", null, null);
+    RealType ir_radiance = new RealType("ir_radiance", null, null);
+    RealType count = new RealType("count", null, null);
+    RealType[] scatter_list = {vis_radiance, ir_radiance, count, RealType.Latitude,
+                               RealType.Longitude, RealType.Radius};
+    RealTupleType scatter = new RealTupleType(scatter_list);
+    RealType[] time = {RealType.Time};
+    RealTupleType time_type = new RealTupleType(time);
+    FunctionType scatter_function = new FunctionType(time_type, scatter);
+
+    int size = 64;
+
+    FlatField imaget1;
+    imaget1 = FlatField.makeField(scatter_function, size, false);
+
+    DisplayImpl display1;
+    
+    GraphicsEnvironment ge = 
+      GraphicsEnvironment.getLocalGraphicsEnvironment();
+    
+    GraphicsDevice gd = ge.getDefaultScreenDevice();
+    
+    GraphicsConfigTemplate3D gct3d = new GraphicsConfigTemplate3D();
+    gct3d.setStereo(GraphicsConfigTemplate3D.REQUIRED);
+    
+    GraphicsConfiguration config = 
+    gct3d.getBestConfiguration(gd.getConfigurations());
+
+    if (config == null)
+    {
+        System.err.println("Unable to find a Stereo visual");
+        System.exit(1);
+    }
+
+    display1 = new DisplayImplJ3D("display1", DisplayImplJ3D.APPLETFRAME, 
+                                 config);
+
+    display1.addMap(new ScalarMap(RealType.Latitude, Display.YAxis));
+    display1.addMap(new ScalarMap(RealType.Longitude, Display.Green));
+    display1.addMap(new ScalarMap(vis_radiance, Display.ZAxis));
+    display1.addMap(new ScalarMap(ir_radiance, Display.XAxis));
+    display1.addMap(new ConstantMap(0.5, Display.Blue));
+    display1.addMap(new ConstantMap(0.5, Display.Red));
+
+    // WLH 28 April 99 - test alpha with points
+    display1.addMap(new ScalarMap(vis_radiance, Display.Alpha));
+
+    GraphicsModeControl mode = display1.getGraphicsModeControl();
+    mode.setPointSize(5.0f);
+
+    DataReferenceImpl ref_imaget1 = new DataReferenceImpl("ref_imaget1");
+    ref_imaget1.setData(imaget1);
+    display1.addReference(ref_imaget1, null);
+
+    DisplayImpl[] dpys = new DisplayImpl[1];
+    dpys[0] = display1;
+
+    return dpys;
+  }
+
+  public String toString() { return ": scatter diagram"; }
+
+  public static void main(String args[])
+	throws VisADException, RemoteException
+  {
+    TestStereo t = new TestStereo(args);
+  }
+}
+
