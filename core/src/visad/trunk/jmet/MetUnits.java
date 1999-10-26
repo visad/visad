914d8c0b22af2a1d48586a9dc0c6e839df12e96f
Tom Whittaker <whittaker@wisc.edu>
1999-10-26 15:06:14 +0000

    Make this a public class and create the "mb" and "hPa" units in the constructor.  Add a method for converting symbols (makeSymbol) that can be used in Parser.parse().

--- core/src/visad/trunk/jmet/MetUnits.java
+++ core/src/visad/trunk/jmet/MetUnits.java
@@ -4,9 +4,9 @@ import visad.*;
 import visad.data.netcdf.*;
 import visad.data.netcdf.units.*;
 
-public final class MetUnits {
+public class MetUnits {
 
-  public static void make() throws Exception {
+  public MetUnits () throws Exception {
     UnitsDB du = DefaultUnitsDB.instance();
     Unit hpa = du.get("hPa");
     hpa = hpa.clone("hPa");
@@ -14,10 +14,30 @@ public final class MetUnits {
     du.putSymbol("hPa", hpa);
     du.putSymbol("MB", hpa);
     du.putSymbol("mb", hpa);
+  }
 
-    Unit meter = du.get("m");
-    meter = meter.clone("m");
-    du.putSymbol("M", meter);
+  public String makeSymbol(String s) {
+    String in = s.trim();
+    String out = in;
+    if (in.equalsIgnoreCase("m")) out = "m";
+    if (in.equalsIgnoreCase("mps")) out = "m/s";
+    if (in.equalsIgnoreCase("mph")) out = "mi/h";
+    if (in.equalsIgnoreCase("km")) out = "km";
+    if (in.equalsIgnoreCase("cm")) out = "cm";
+    if (in.equalsIgnoreCase("mi")) out = "mi";
+    if (in.equalsIgnoreCase("nmi")) out = "nmi";
+    if (in.equalsIgnoreCase("mm")) out = "mm";
+    if (in.equalsIgnoreCase("yd")) out = "yd";
+    if (in.equalsIgnoreCase("ft")) out = "ft";
+    if (in.equalsIgnoreCase("f")) out = "degF";
+    if (in.equalsIgnoreCase("c")) out = "defC";
+    if (in.equalsIgnoreCase("k")) out = "K";
+    if (in.equalsIgnoreCase("inhg")) out = "inhg";
+    if (in.equalsIgnoreCase("kt")) out = "kt";
+    if (in.equalsIgnoreCase("kts")) out = "kt";
+    if (in.equalsIgnoreCase("g/kg")) out = "g/kg";
+
+    return out;
   }
 }
 
