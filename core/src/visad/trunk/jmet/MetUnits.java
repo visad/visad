32c865dd98a66ab72ae29afb009dc03cacdb1853
Tom Whittaker <whittaker@wisc.edu>
1999-11-10 16:54:56 +0000

    Add a bunch of "McIDAS" names

--- core/src/visad/trunk/jmet/MetUnits.java
+++ core/src/visad/trunk/jmet/MetUnits.java
@@ -20,13 +20,16 @@ public class MetUnits {
     String in = s.trim();
     String out = in;
     if (in.equalsIgnoreCase("m")) out = "m";
+    if (in.equalsIgnoreCase("sec")) out = "s";
     if (in.equalsIgnoreCase("mps")) out = "m/s";
     if (in.equalsIgnoreCase("mph")) out = "mi/h";
     if (in.equalsIgnoreCase("km")) out = "km";
     if (in.equalsIgnoreCase("cm")) out = "cm";
     if (in.equalsIgnoreCase("mi")) out = "mi";
+    if (in.equalsIgnoreCase("pa")) out = "Pa";
     if (in.equalsIgnoreCase("nmi")) out = "nmi";
     if (in.equalsIgnoreCase("mm")) out = "mm";
+    if (in.equalsIgnoreCase("deg")) out = "deg";
     if (in.equalsIgnoreCase("yd")) out = "yd";
     if (in.equalsIgnoreCase("ft")) out = "ft";
     if (in.equalsIgnoreCase("f")) out = "degF";
@@ -36,6 +39,26 @@ public class MetUnits {
     if (in.equalsIgnoreCase("kt")) out = "kt";
     if (in.equalsIgnoreCase("kts")) out = "kt";
     if (in.equalsIgnoreCase("g/kg")) out = "g/kg";
+
+    // the following are decidedly McIDAS
+    if (in.equalsIgnoreCase("paps")) out = "Pa/s";
+    if (in.equalsIgnoreCase("mgps")) out = "km^2(kg/s)";
+    if (in.equalsIgnoreCase("m2s2")) out = "m^2/s^2";
+    if (in.equalsIgnoreCase("kpm")) out = "K/m";
+    if (in.equalsIgnoreCase("m2ps")) out = "m^2/s";
+    if (in.equalsIgnoreCase("gpkg")) out = "g/kg";
+    if (in.equalsIgnoreCase("kgm2")) out = "kg/m^2";
+    if (in.equalsIgnoreCase("kgm3")) out = "kg/m^3";
+    if (in.equalsIgnoreCase("mmps")) out = "(kg/m^2)/s";
+    if (in.equalsIgnoreCase("ps")) out = "s-1";
+    if (in.equalsIgnoreCase("wpm2")) out = "W/m^2";
+    if (in.equalsIgnoreCase("nm2")) out = "N/m^2";
+    if (in.equalsIgnoreCase("kgps")) out = "kg/kg/s";
+    if (in.equalsIgnoreCase("ps2")) out = "s^-2";
+    if (in.equalsIgnoreCase("pspm")) out = "s^-1m^-1";
+    if (in.equalsIgnoreCase("jpkg")) out = "J/kg";
+    if (in.equalsIgnoreCase("kgkg")) out = "kg/kg";
+    if (in.equalsIgnoreCase("psps")) out = "s^-1";
 
     return out;
   }
