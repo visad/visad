60f179dea5d8cc219e5a413f42eeb633f56b797f
Dave Glowacki <dglo@ssec.wisc.edu>
1999-11-12 20:00:27 +0000

    Rework this a bit:   * Switch between 3 background colors   * Instead of printing "delay", print the background/box/cursor color names   * Enable client/server mode

--- core/src/visad/trunk/examples/Test53.java
+++ core/src/visad/trunk/examples/Test53.java
@@ -41,7 +41,7 @@ import visad.java2d.DisplayImplJ2D;
 public class Test53
   extends UISkeleton
 {
-  boolean hasClientServerMode() { return false; }
+  private boolean isServer = false;
 
   public Test53() { }
 
@@ -76,22 +76,66 @@ public class Test53
     dpys[0].addMap(new ConstantMap(1.0, Display.Green));
     dpys[0].addMap(new ConstantMap(0.0, Display.Blue));
 
-    dpys[0].getDisplayRenderer().setBackgroundColor(1.0f, 0.0f, 1.0f);
-
     DataReferenceImpl ref_histogram1;
     ref_histogram1 = new DataReferenceImpl("ref_histogram1");
     ref_histogram1.setData(histogram1);
     dpys[0].addReference(ref_histogram1, null);
+
+    isServer = true;
   }
 
+  private static String colorName(float[] color)
+  {
+    int red = (int )(256.0f * color[0]) / 64;
+    int green = (int )(256.0f * color[1]) / 64;
+    int blue = (int )(256.0f * color[2]) / 64;
+
+    if (red == green && green == blue) {
+      switch (red) {
+      case 0: return "black";
+      case 1: return "dark gray";
+      case 2: return "light gray";
+      default: return "white";
+      }
+    }
+    if (red > 0) {
+      if (green > 0) {
+        if (blue > 0) {
+        } else {
+          return "yellow";
+        }
+      } else {                    // green == 0
+        if (blue > 0) {   // green == 0
+          return "magenta";
+        } else {
+          return "red";
+        }
+      }
+    } else {                             // red == 0
+      if (green > 0) {
+        if (blue > 0) {
+          return "cyan";
+        } else {
+          return "green";
+        }
+      } else {
+        if (blue > 0) {
+          return "blue";
+        }
+      }
+    }
+
+    return "color[" + color[0] + "/" + color[1] + "/" + color[2] + "]";
+  }
 
   void setupUI(LocalDisplay[] dpys)
     throws RemoteException, VisADException
   {
     super.setupUI(dpys);
 
-    boolean forever = true;
-    boolean[] box_on = {true, true, true, false};
+    float[][] bg_color = {{1.0f, 0.0f, 1.0f},
+                          {1.0f, 1.0f, 0.0f},
+                          {0.0f, 1.0f, 1.0f}};
     float[][] box_color = {{1.0f, 0.0f, 0.0f},
                            {0.0f, 1.0f, 0.0f},
                            {0.0f, 0.0f, 1.0f},
@@ -101,24 +145,43 @@ public class Test53
                               {0.0f, 1.0f, 0.0f},
                               {0.0f, 0.0f, 1.0f}};
     DisplayRenderer displayRenderer = dpys[0].getDisplayRenderer();
-    int index = 0;
-    while (forever) {
+    int i3 = 0;
+    int i4 = 0;
+    while (isServer) {
       // delay(5000);
       try {
         Thread.sleep(5000);
       }
       catch (InterruptedException e) {
       }
-      System.out.println("\ndelay\n");
-      displayRenderer.setBoxOn(box_on[index]);
-      displayRenderer.setBoxColor(box_color[index][0],
-                                  box_color[index][1],
-                                  box_color[index][2]);
-      displayRenderer.setCursorColor(cursor_color[index][0],
-                                     cursor_color[index][1],
-                                     cursor_color[index][2]);
-      index++;
-      if (index > 3) index = 0;
+
+      boolean boxOn = (i3 != 2);
+//      System.out.println("\ndelay\n");
+      System.out.println("\n" +
+                         colorName(bg_color[i3]) + " background, " +
+                         (boxOn ? colorName(box_color[i4]) + " box" :
+                          "box off") + ", " +
+                         colorName(cursor_color[i4]) + " cursor\n");
+      displayRenderer.setBackgroundColor(bg_color[i3][0],
+                                         bg_color[i3][1],
+                                         bg_color[i3][2]);
+      displayRenderer.setBoxOn(boxOn);
+      displayRenderer.setBoxColor(box_color[i4][0],
+                                  box_color[i4][1],
+                                  box_color[i4][2]);
+      displayRenderer.setCursorColor(cursor_color[i4][0],
+                                     cursor_color[i4][1],
+                                     cursor_color[i4][2]);
+      if (i3 == 2) {
+        i3 = 0;
+      } else {
+        i3++;
+      }
+      if (i4 == 3) {
+        i4 = 0;
+      } else {
+        i4++;
+      }
     }
   }
 
