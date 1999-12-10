0dce2100c34a79b5bf0118bd57a2f50798c80afa
Dave Glowacki <dglo@ssec.wisc.edu>
1999-12-10 19:16:41 +0000

    Add constants for ScalarMapControlEvent Add a new constructor which takes the event type as an argument Deprecate the old constructor, which only allows for MANUAL and AUTO_SCALE evts

--- core/src/visad/trunk/ScalarMapEvent.java
+++ core/src/visad/trunk/ScalarMapEvent.java
@@ -41,6 +41,9 @@ public class ScalarMapEvent extends Event {
   public final static int UNKNOWN = 0;
   public final static int AUTO_SCALE = 1;
   public final static int MANUAL = 2;
+  public final static int CONTROL_ADDED = 3;
+  public final static int CONTROL_REMOVED = 4;
+  public final static int CONTROL_REPLACED = 5;
 
   private int id = UNKNOWN;
 
@@ -50,8 +53,27 @@ public class ScalarMapEvent extends Event {
    * Create a <CODE>ScalarMap</CODE> event
    *
    * @param map map to which this event refers
+   * @param id the event type.
+   */
+  public ScalarMapEvent(ScalarMap map, int id)
+  {
+    // don't pass map as the source, since source
+    // is transient inside Event
+    super(null, 0, null);
+    this.map = map;
+    this.id = id;
+  }
+
+  /**
+   * Create a <CODE>ScalarMap</CODE> event
+   *
+   * @param map map to which this event refers
    * @param auto <CODE>true</CODE> if this is an AUTO_SCALE event
    *             <CODE>false</CODE> if it's a MANUAL event.
+   *
+   * @deprecated - Explicitly cite the event ID using the
+   *               <CODE>ScalarMapEvent(ScalarMap map, int id)</CODE>
+   *               constructor.
    */
   public ScalarMapEvent(ScalarMap map, boolean auto)
   {
@@ -80,6 +102,9 @@ public class ScalarMapEvent extends Event {
    *         <UL>
    *           <LI>ScalarMapEvent.AUTO_SCALE
    *           <LI>ScalarMapEvent.MANUAL
+   *           <LI>ScalarMapEvent.CONTROL_ADDED
+   *           <LI>ScalarMapEvent.CONTROL_REMOVED
+   *           <LI>ScalarMapEvent.CONTROL_REPLACED
    *         </UL>
    */
   public int getId()
@@ -97,6 +122,9 @@ public class ScalarMapEvent extends Event {
     switch (id) {
     case AUTO_SCALE: return "AUTO_SCALE";
     case MANUAL: return "MANUAL";
+    case CONTROL_ADDED: return "CONTROL_ADDED";
+    case CONTROL_REMOVED: return "CONTROL_REMOVED";
+    case CONTROL_REPLACED: return "CONTROL_REPLACED";
     }
 
     return "UNKNOWN_ID=" + id;
