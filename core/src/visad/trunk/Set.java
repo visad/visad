ae8fc6538c979a8a5703dffa4946ea13e499cd0c
Bill Hibbard <billh@ssec.wisc.edu>
1999-12-28 18:43:48 +0000

    fix ancient lurking bug

--- core/src/visad/trunk/Set.java
+++ core/src/visad/trunk/Set.java
@@ -343,7 +343,9 @@ public abstract class Set extends DataImpl {
     int set_length = set.getLength();
     boolean[] set_indices = new boolean[set_length];
     for (int i=0; i<set_length; i++) set_indices[i] = true;
-    for (int i=0; i<length; i++) set_indices[test_indices[i]] = false;
+    for (int i=0; i<length; i++) {
+      if (test_indices[i] > -1) set_indices[test_indices[i]] = false;
+    }
     // now set_indices = true for indices of set not covered by this
     int num_new = 0;
     for (int i=0; i<set_length; i++) if (set_indices[i]) num_new++;
