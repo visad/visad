ca53c20b56a3e7345d5229e767e2417500814d26
Dave Glowacki <dglo@ssec.wisc.edu>
2000-03-13 19:59:15 +0000

    Reindent

--- core/src/visad/trunk/util/ColorMap.java
+++ core/src/visad/trunk/util/ColorMap.java
@@ -1,18 +1,18 @@
 /*
 
-@(#) $Id: ColorMap.java,v 1.8 2000-02-24 16:49:29 dglo Exp $
+@(#) $Id: ColorMap.java,v 1.9 2000-03-13 19:59:14 dglo Exp $
 
 VisAD Utility Library: Widgets for use in building applications with
 the VisAD interactive analysis and visualization library
 Copyright (C) 1998 Nick Rasmussen
 VisAD is Copyright (C) 1996 - 1998 Bill Hibbard, Curtis Rueden, Tom
 Rink and Dave Glowacki.
- 
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 1, or (at your option)
 any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -31,86 +31,85 @@ import java.util.Vector;
 
 import javax.swing.JPanel;
 
-/** 
- * The abstract class that all color-mapping widgets must extend.  This 
+/**
+ * The abstract class that all color-mapping widgets must extend.  This
  * class manages all of the listener notification for the ColorMaps.
  *
  * @author Nick Rasmussen nick@cae.wisc.edu
- * @version $Revision 1.7 $, $Date: 2000-02-24 16:49:29 $
+ * @version $Revision 1.7 $, $Date: 2000-03-13 19:59:14 $
  * @since Visad Utility Library, 0.5
  */
 
 public abstract class ColorMap extends JPanel {
 
-	/** Maps a floating point value (in the range 0 to 1) onto a Color, 
-	 * returns Color.black if the number is out of range
-	 */
-	public Color getColor(float value) {
-		if (value < 0 || value > 1) {
-			return Color.black;
-		}
-		
-		float[] rgb = getRGBTuple(value);
-		
-		if (rgb[0] < 0) rgb[0] = 0;
-		if (rgb[1] < 0) rgb[1] = 0;
-		if (rgb[2] < 0) rgb[2] = 0;
-		if (rgb[0] > 1) rgb[0] = 1;
-		if (rgb[1] > 1) rgb[1] = 1;
-		if (rgb[2] > 1) rgb[2] = 1;
-		
-		return new Color(rgb[0], rgb[1], rgb[2]);
-	}
-	
-	/** Maps a floating point value (in the range 0 to 1) onto an RGB
-	 * triplet of floating point numbers in the range 0 to 1)
-	 */
-	public abstract float[] getRGBTuple(float value);
-	
-	/** Maps a floating point value (in the range 0 to 1) into a tuple
-	 * with dimension of the map */
-	public abstract float[] getTuple(float value);
-	
-	/** Returns the current map resolution */
-	public abstract int getMapResolution();
-	
-	/** Returns the dimension of the map */
-	public abstract int getMapDimension();
-	
-	/** Returns a copy of the ColorMap */
-	public abstract float[][] getColorMap();
-	
-	/** The vector containing the ColorChangeListeners */
-	private Vector listeners = new Vector();
-	
-	/** Add a ColorChangeListener to the listeners list */
-	public void addColorChangeListener(ColorChangeListener c) {
-          synchronized (listeners) {
-		if (!listeners.contains(c)) {
-			listeners.addElement(c);
-		}
-          }
-	}
-	
-	/** Remove a ColorChangeListener from the listeners list */
-	public void removeColorChangeListener(ColorChangeListener c) {
-          synchronized (listeners) {
-		if (listeners.contains(c)) {
-			listeners.removeElement(c);
-		}
-          }
-	}
-	
-	/** Notify the ColorChangeListerers that the color widget has changed */
-	protected void notifyListeners(ColorChangeEvent e) {
-          Vector cl = null;
-          synchronized (listeners) {
-            cl = (Vector) listeners.clone();
-          }
-          for (int i = 0; i < cl.size(); i++) {
-            ColorChangeListener c = (ColorChangeListener) cl.elementAt(i);
-            c.colorChanged(e);
-          }
-	}
-}
+  /** Maps a floating point value (in the range 0 to 1) onto a Color,
+   * returns Color.black if the number is out of range
+   */
+  public Color getColor(float value) {
+    if (value < 0 || value > 1) {
+      return Color.black;
+    }
 
+    float[] rgb = getRGBTuple(value);
+
+    if (rgb[0] < 0) rgb[0] = 0;
+    if (rgb[1] < 0) rgb[1] = 0;
+    if (rgb[2] < 0) rgb[2] = 0;
+    if (rgb[0] > 1) rgb[0] = 1;
+    if (rgb[1] > 1) rgb[1] = 1;
+    if (rgb[2] > 1) rgb[2] = 1;
+
+    return new Color(rgb[0], rgb[1], rgb[2]);
+  }
+
+  /** Maps a floating point value (in the range 0 to 1) onto an RGB
+   * triplet of floating point numbers in the range 0 to 1)
+   */
+  public abstract float[] getRGBTuple(float value);
+
+  /** Maps a floating point value (in the range 0 to 1) into a tuple
+   * with dimension of the map */
+  public abstract float[] getTuple(float value);
+
+  /** Returns the current map resolution */
+  public abstract int getMapResolution();
+
+  /** Returns the dimension of the map */
+  public abstract int getMapDimension();
+
+  /** Returns a copy of the ColorMap */
+  public abstract float[][] getColorMap();
+
+  /** The vector containing the ColorChangeListeners */
+  private Vector listeners = new Vector();
+
+  /** Add a ColorChangeListener to the listeners list */
+  public void addColorChangeListener(ColorChangeListener c) {
+    synchronized (listeners) {
+      if (!listeners.contains(c)) {
+        listeners.addElement(c);
+      }
+    }
+  }
+
+  /** Remove a ColorChangeListener from the listeners list */
+  public void removeColorChangeListener(ColorChangeListener c) {
+    synchronized (listeners) {
+      if (listeners.contains(c)) {
+        listeners.removeElement(c);
+      }
+    }
+  }
+
+  /** Notify the ColorChangeListerers that the color widget has changed */
+  protected void notifyListeners(ColorChangeEvent e) {
+    Vector cl = null;
+    synchronized (listeners) {
+      cl = (Vector) listeners.clone();
+    }
+    for (int i = 0; i < cl.size(); i++) {
+      ColorChangeListener c = (ColorChangeListener) cl.elementAt(i);
+      c.colorChanged(e);
+    }
+  }
+}
--- core/src/visad/trunk/util/ColorPreview.java
+++ core/src/visad/trunk/util/ColorPreview.java
@@ -1,18 +1,18 @@
 /*
 
-@(#) $Id: ColorPreview.java,v 1.10 2000-02-24 16:49:29 dglo Exp $
+@(#) $Id: ColorPreview.java,v 1.11 2000-03-13 19:59:15 dglo Exp $
 
 VisAD Utility Library: Widgets for use in building applications with
 the VisAD interactive analysis and visualization library
 Copyright (C) 1998 Nick Rasmussen
 VisAD is Copyright (C) 1996 - 1998 Bill Hibbard, Curtis Rueden, Tom
 Rink and Dave Glowacki.
- 
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 1, or (at your option)
 any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -30,148 +30,148 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-/** 
+/**
  * A small preview bar generated for a color widget
  *
  * @author Nick Rasmussen nick@cae.wisc.edu
- * @version $Revision: 1.10 $, $Date: 2000-02-24 16:49:29 $
+ * @version $Revision: 1.11 $, $Date: 2000-03-13 19:59:15 $
  * @since Visad Utility Library, 0.5
  */
 
 public class ColorPreview extends JPanel implements ColorChangeListener {
 
-	/** The ColorWidget that this is attached to */
-	private ColorMap map;
-	
-	/** The height of the ColorPreview */
-	private int height;
+  /** The ColorWidget that this is attached to */
+  private ColorMap map;
 
-	/**
-         * Constructs a ColorPreview that Listens to the specified
-         * widget and has the default height
-         *
-         * @deprecated Specify the ColorMap instead.
-	 */
-	ColorPreview(ColorWidget widget) {
-		this(widget, 15);
-	}
+  /** The height of the ColorPreview */
+  private int height;
 
-	/**
-         * Constructs a ColorPreview that listens to the specified
-         * ColorWidget and has the specified height
-         *
-         * @deprecated Specify the ColorMap instead.
-	 */
-	public ColorPreview(ColorWidget widget, int height) {
-		this(widget.getColorMap(), height);
-        }
+  /**
+   * Constructs a ColorPreview that Listens to the specified
+   * widget and has the default height
+   *
+   * @deprecated Specify the ColorMap instead.
+   */
+  ColorPreview(ColorWidget widget) {
+    this(widget, 15);
+  }
 
-	/**
-         * Constructs a ColorPreview that Listens to the specified
-         * ColorMap and has the default height
-	 */
-	ColorPreview(ColorMap map) {
-		this(map, 15);
-	}
+  /**
+   * Constructs a ColorPreview that listens to the specified
+   * ColorWidget and has the specified height
+   *
+   * @deprecated Specify the ColorMap instead.
+   */
+  public ColorPreview(ColorWidget widget, int height) {
+    this(widget.getColorMap(), height);
+  }
 
-	/**
-         * Constructs a ColorPreview that listens to the specified
-         * ColorMap and has the specified height
-	 */
-	public ColorPreview(ColorMap map, int height) {
-		this.map = map;
-		this.height = height;
-		map.addColorChangeListener(this);
-	}
+  /**
+   * Constructs a ColorPreview that Listens to the specified
+   * ColorMap and has the default height
+   */
+  ColorPreview(ColorMap map) {
+    this(map, 15);
+  }
 
-	/** Overridden to maintain the preview at the specified height */
-	public Dimension getMaximumSize() {
-		return new Dimension(Integer.MAX_VALUE, height);
-	}
+  /**
+   * Constructs a ColorPreview that listens to the specified
+   * ColorMap and has the specified height
+   */
+  public ColorPreview(ColorMap map, int height) {
+    this.map = map;
+    this.height = height;
+    map.addColorChangeListener(this);
+  }
 
-	/** Redraw the entire panel */	
-	public void paint(Graphics g) {
-		updateLeft = 0;
-		updateRight = 1;
-		update(g);
-	}
+  /** Overridden to maintain the preview at the specified height */
+  public Dimension getMaximumSize() {
+    return new Dimension(Integer.MAX_VALUE, height);
+  }
 
-	/** The location to begin an update */	
-	private float updateLeft;
-	/** The location to end an update */
-	private float updateRight;
+  /** Redraw the entire panel */
+  public void paint(Graphics g) {
+    updateLeft = 0;
+    updateRight = 1;
+    update(g);
+  }
 
-	/** Updates the nessecary areas of the panel after ColorChangeEvents and paint()
-	 * @see ColorChangeEvent
-	 */
-	public void update(Graphics g) {
-	
-		int leftIndex;
-		int rightIndex;
-	
-		synchronized(this) {
-			leftIndex = (int) Math.floor(updateLeft * getBounds().width);
-			rightIndex = (int) Math.floor(updateRight * getBounds().width);
-			updateLeft = 1;
-			updateRight = 0;
-		}
-		
-		if (leftIndex > rightIndex) {
-			int tmp = leftIndex;
-			leftIndex = rightIndex;
-			rightIndex = tmp;
-		}
+  /** The location to begin an update */
+  private float updateLeft;
+  /** The location to end an update */
+  private float updateRight;
 
-		if (leftIndex < 0) {
-			leftIndex = 0;
-		}
-		if (leftIndex >= getBounds().width) {
-			leftIndex = getBounds().width - 1;
-		}
-		if (rightIndex < 0) {
-			rightIndex = 0;
-		}
-		if (rightIndex >= getBounds().width) {
-			rightIndex = getBounds().width - 1;
-		}
+  /** Updates the nessecary areas of the panel after ColorChangeEvents and paint()
+   * @see ColorChangeEvent
+   */
+  public void update(Graphics g) {
 
-		for (int i = leftIndex; i <= rightIndex; i++) {
-			float percent = (float) i / (float) getBounds().width;
-			g.setColor(map.getColor(percent));
-			g.drawLine(i,0,i,getBounds().height - 1);
-		}
-	}
+    int leftIndex;
+    int rightIndex;
 
-	/** Implementation of the ColorChangeListener interface
-	 * @see ColorChangeListener
-	 */
-	public void colorChanged(ColorChangeEvent e) {
-		synchronized(this) {
-			if (e.getStart() < updateLeft) {
-				updateLeft = e.getStart();
-			}
-			if (e.getEnd() > updateRight) {
-				updateRight = e.getEnd();
-			}
-		}
+    synchronized(this) {
+      leftIndex = (int) Math.floor(updateLeft * getBounds().width);
+      rightIndex = (int) Math.floor(updateRight * getBounds().width);
+      updateLeft = 1;
+      updateRight = 0;
+    }
 
-                // redraw
-                validate();
-		repaint();	
-	}
+    if (leftIndex > rightIndex) {
+      int tmp = leftIndex;
+      leftIndex = rightIndex;
+      rightIndex = tmp;
+    }
 
-	/** Finds the preferred width of the ColorMap, and returns it with the specified
-	 * height
-	 */
-	public Dimension getPreferredSize() {
-		Dimension d = map.getPreferredSize();
-		return new Dimension(d.width, height);
-	}
+    if (leftIndex < 0) {
+      leftIndex = 0;
+    }
+    if (leftIndex >= getBounds().width) {
+      leftIndex = getBounds().width - 1;
+    }
+    if (rightIndex < 0) {
+      rightIndex = 0;
+    }
+    if (rightIndex >= getBounds().width) {
+      rightIndex = getBounds().width - 1;
+    }
 
-	public void setMap(ColorMap newMap)
-	{
-	  map.removeColorChangeListener(this);
-	  newMap.addColorChangeListener(this);
-	  map = newMap;
-	}
+    for (int i = leftIndex; i <= rightIndex; i++) {
+      float percent = (float) i / (float) getBounds().width;
+      g.setColor(map.getColor(percent));
+      g.drawLine(i,0,i,getBounds().height - 1);
+    }
+  }
+
+  /** Implementation of the ColorChangeListener interface
+   * @see ColorChangeListener
+   */
+  public void colorChanged(ColorChangeEvent e) {
+    synchronized(this) {
+      if (e.getStart() < updateLeft) {
+        updateLeft = e.getStart();
+      }
+      if (e.getEnd() > updateRight) {
+        updateRight = e.getEnd();
+      }
+    }
+
+    // redraw
+    validate();
+    repaint();
+  }
+
+  /** Finds the preferred width of the ColorMap, and returns it with the specified
+   * height
+   */
+  public Dimension getPreferredSize() {
+    Dimension d = map.getPreferredSize();
+    return new Dimension(d.width, height);
+  }
+
+  public void setMap(ColorMap newMap)
+  {
+    map.removeColorChangeListener(this);
+    newMap.addColorChangeListener(this);
+    map = newMap;
+  }
 }
