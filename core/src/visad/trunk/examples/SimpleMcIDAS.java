b7991bb4baf982bd63043bafa9d3a07c249ebab6
Bill Hibbard <billh@ssec.wisc.edu>
2000-04-07 13:58:25 +0000

    make pretty

--- core/src/visad/trunk/examples/SimpleMcIDAS.java
+++ core/src/visad/trunk/examples/SimpleMcIDAS.java
@@ -1,5 +1,7 @@
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import visad.*;
@@ -14,35 +16,42 @@ public class SimpleMcIDAS {
   public static void main (String[] args)
          throws VisADException, RemoteException, IOException {
 
+    // construct a 3-D display
     DisplayImpl display = new DisplayImplJ3D("display");
+
+    // read McIDAS AREA file
+    AreaAdapter areaAdapter = new AreaAdapter(args[0]);
+    Data image = areaAdapter.getData();
+
+    // get type of image radiance
+    FunctionType imageFunctionType = (FunctionType) image.getType();
+    RealType radianceType = (RealType)
+      ((RealTupleType) imageFunctionType.getRange()).getComponent(0);
+
+    // define display coordinates
     display.addMap(new ScalarMap(RealType.Latitude, Display.Latitude));
     display.addMap(new ScalarMap(RealType.Longitude, Display.Longitude));
-
-    AreaAdapter areaAdapter = new AreaAdapter(args[0]);
-    FlatField image = areaAdapter.getData();
-
-    FunctionType imageFunctionType = (FunctionType) image.getType();
-    RealTupleType imageRangeType = 
-      (RealTupleType) imageFunctionType.getRange();
-
-    ScalarMap rgbMap =
-      new ScalarMap((RealType) imageRangeType.getComponent(0), Display.RGB);
+    ScalarMap rgbMap = new ScalarMap(radianceType, Display.RGB);
     display.addMap(rgbMap);
 
-    DataReferenceImpl imageRef = new DataReferenceImpl("ImageRef");
-    imageRef.setData(image);
-
+    // read McIDAS map file
     BaseMapAdapter baseMapAdapter = new BaseMapAdapter(args[1]);
     Data map = baseMapAdapter.getData();
+
+    // link map to display
     DataReference maplinesRef = new DataReferenceImpl("MapLines");
     maplinesRef.setData(map);
     ConstantMap[] maplinesConstantMap = new ConstantMap[]
-      {new ConstantMap(1.001, Display.Radius),
+      {new ConstantMap(1.002, Display.Radius),
        new ConstantMap(0.0, Display.Blue)};
-
     display.addReference(maplinesRef, maplinesConstantMap);
+
+    // link image to display
+    DataReferenceImpl imageRef = new DataReferenceImpl("ImageRef");
+    imageRef.setData(image);
     display.addReferences(new ImageRendererJ3D(), imageRef);
 
+    // create frame (window) on screen
     JFrame frame = new JFrame("Satellite Display");
     frame.addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
@@ -50,13 +59,30 @@ public class SimpleMcIDAS {
       }
     });
 
+    // add 3-D display to panel in frame
     JPanel panel = new JPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     panel.add(display.getComponent());
-    panel.add(new LabeledColorWidget(new ColorMapWidget(rgbMap, false)));
+
+    // construct color widget for image radiances
+    LabeledColorWidget lcw =
+      new LabeledColorWidget(new ColorMapWidget(rgbMap, false));
+
+    // add color widget to panel in frame
+    JPanel sub_panel = new JPanel();
+    sub_panel.setMaximumSize(new Dimension(500, 150));
+    sub_panel.setBorder(new CompoundBorder(new EtchedBorder(),
+                        new EmptyBorder(5, 5, 5, 5)));
+    sub_panel.add(lcw);
+    panel.add(sub_panel);
+
+    // finish off frame
     frame.getContentPane().add(panel);
     frame.setSize(500, 700);
     frame.setVisible(true);
+
+    // and now exit - leaving display, color widget,
+    // map and image to take care of themselves
   }
 }
 
