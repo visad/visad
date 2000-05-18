c6fb7964d6b80a2bd3eaa4b7ec0c107a3c03db43
Bill Hibbard <billh@ssec.wisc.edu>
2000-05-18 22:59:03 +0000

    add comments

--- core/src/visad/trunk/bom/TrackManipulation.java
+++ core/src/visad/trunk/bom/TrackManipulation.java
@@ -71,19 +71,28 @@ public class TrackManipulation extends Object {
   /** (lat1, lon1) start of track
       (lat2, lon2) end of track
       d is a DisplayImplJ3D that has ScalarMaps of Latitude and
-      Longitude but is not linked yet to any DataReferences */
+      Longitude but is not linked yet to any DataReferences;
+      this constructor will add another ScalarMap to Shape and
+      link the DisplayImplJ3D d to three data objects, two by
+      direct manipulation */
   public TrackManipulation(float lat1, float lon1, float lat2, float lon2,
                            DisplayImplJ3D d, float xs, float ys, float ang)
          throws VisADException, RemoteException {
 
+    // construct RealTuple objects to be manipulated at ends of track
     RealTupleType latlon =
       new RealTupleType(RealType.Latitude, RealType.Longitude);
     RealTuple track1 = new RealTuple(latlon, new double[] {lat1, lon1});
     RealTuple track2 = new RealTuple(latlon, new double[] {lat2, lon2});
+
+    // construct RealTuple with RealType mapped to Shape that
+    // creates storm track depiction
     RealType shape = new RealType("shape");
     latlonshape =
       new RealTupleType(RealType.Latitude, RealType.Longitude, shape);
     RealTuple track3 = new RealTuple(latlonshape, new double[] {lat1, lon1, 0.0});
+
+    // construct DataReferences for these three RealTuples
     track1_ref = new DataReferenceImpl("track1_ref");
     track2_ref = new DataReferenceImpl("track2_ref");
     track3_ref = new DataReferenceImpl("track3_ref");
@@ -91,8 +100,11 @@ public class TrackManipulation extends Object {
     track2_ref.setData(track2);
     track3_ref.setData(track3);
 
+    // copy reference to DisplayImplJ3D
     display = d;
 
+    // compute basic ellipse shape according to constructor
+    // arguments
     x_size = (float) Math.abs(xs);
     y_size = (float) Math.abs(ys);
     angle = ang;
@@ -106,6 +118,7 @@ public class TrackManipulation extends Object {
       y_ellipse[i] = ca * ye - sa * xe;
     }
 
+    // find ScalarMaps of Latitude and Longitude
     Vector scalar_map_vector = display.getMapVector();
     Enumeration enum = scalar_map_vector.elements();
     while (enum.hasMoreElements()) {
@@ -121,6 +134,10 @@ public class TrackManipulation extends Object {
       throw new DisplayException("Latitude and Longitude must be mapped " +
                                  "in display");
     }
+
+    // get information from latmap and lonmap needed to
+    // compute display locations from manipulated RealTuple
+    // values
     DisplayRealType latreal = latmap.getDisplayScalar();
     DisplayRealType lonreal = lonmap.getDisplayScalar();
     DisplayTupleType lattuple = latreal.getTuple();
@@ -136,29 +153,33 @@ public class TrackManipulation extends Object {
       (DisplayRealType) lattuple.getComponent(otherindex);
     othervalue = (float) othertype.getDefaultValue();
     coord = lattuple.getCoordinateSystem();
-    if (lattuple.equals(Display.DisplaySpatialCartesianTuple)) {
-    }
-    else if (coord != null &&
-             coord.getReference().equals(Display.DisplaySpatialCartesianTuple)) {
-    }
-    else {
+    if (!lattuple.equals(Display.DisplaySpatialCartesianTuple) &&
+        !(coord != null &&
+          coord.getReference().equals(Display.DisplaySpatialCartesianTuple))) {
       throw new DisplayException("Latitude and Longitude must be mapped " +
                                  "to spatial in display(2)");
     }
+
+    // construct ScalarMap to Shape, with a single 'shape'
     shapemap = new ScalarMap(shape, Display.Shape);
     display.addMap(shapemap);
     shapecontrol = (ShapeControl) shapemap.getControl();
     shapecontrol.setShapeSet(new Integer1DSet(1));
 
+    // link RealTuples to display
     display.addReferences(new DirectManipulationRendererJ3D(), track1_ref);
     display.addReferences(new DirectManipulationRendererJ3D(), track2_ref);
     display.addReference(track3_ref);
 
+    // construct CellImpl that computes storm track shape in
+    // response to user manipulation of track end points, and
+    // link it to those manipulable RealTuples
     TrackGetter tracker = new TrackGetter();
     tracker.addReference(track1_ref);
     tracker.addReference(track2_ref);
   }
 
+  // compute approximate radius of ellipse in direction (x, y)
   private float getStep(float x, float y) {
     double dist = 2.0 * (x_size + y_size);
     float step = -1.0f;
@@ -173,30 +194,37 @@ public class TrackManipulation extends Object {
     return step;
   }
 
+  // maximum size of shape geometry
   private static final int NUM = 4096;
 
-  /** draw track */
+  /** construct a VisADLineArray in the shape of a storm track
+      with end points given in the values array */
   private VisADLineArray makeTrack(float[][] values) {
     float d, xd, yd;
     float x, y, z, x0, y0, x3, y3, x4, y4, x5, y5;
     float sscale = 0.75f * 0.15f;
 
+    // start of arrow is located at first manipulable RealTuple
     x = 0.0f;
     y = 0.0f;
     z = values[2][0];
+    // head of arrow is located at second manipulable RealTuple
     x5 = values[0][1] - values[0][0];
     y5 = values[1][1] - values[1][0];
 
+    // get arrow vector and length
     float xdir = x5 - x;
     float ydir = y5 - y; 
     float dist = (float) Math.sqrt(xdir * xdir + ydir * ydir);
-
-    int nv = 0;
 
     // normalize direction
     x0 = xdir / dist;
     y0 = ydir / dist;
 
+    // running count of geometry size
+    int nv = 0;
+
+    // allocate arrays for geometry
     float[] vx = new float[NUM];
     float[] vy = new float[NUM];
     float[] vz = new float[NUM];
@@ -212,9 +240,9 @@ public class TrackManipulation extends Object {
     vz[nv] = z;
     nv++;
 
+    // computation for arrow head
     xd = sscale * x0;
     yd = sscale * y0;
-
     x3 = x5 - 0.3f * (xd - yd);
     y3 = y5 - 0.3f * (yd + xd);
     x4 = x5 - 0.3f * (xd + yd);
@@ -239,6 +267,7 @@ public class TrackManipulation extends Object {
     vz[nv] = z;
     nv++;
 
+    // compute number of ellipses and spacing between them
     float step = getStep(xdir, ydir); 
     int nsteps = (int) (dist / step);
     if (nsteps < 1) nsteps = 1;
@@ -248,6 +277,8 @@ public class TrackManipulation extends Object {
     float xstep = xdir / nsteps;
     float ystep = ydir / nsteps;
 
+    // compute which ellipse points are 'outside' previous
+    // ellipse (and hence visible)
     boolean[] outside = new boolean[NE + 1];
     for (int i=0; i<NE+1; i++) {
       float xs = x_ellipse[i] + xstep;
@@ -257,6 +288,7 @@ public class TrackManipulation extends Object {
       outside[i] = (len > radius);
     }
 
+    // construct geometry for visible points of one ellipse
     float[] xe = new float[2 * NE];
     float[] ye = new float[2 * NE];
     int ne = 0;
@@ -271,6 +303,7 @@ public class TrackManipulation extends Object {
       }
     }
 
+    // add sequence of ellipses to storm track geometry
     float xcenter = x;
     float ycenter = y;
     for (int i=0; i<nsteps; i++) {
@@ -284,6 +317,7 @@ public class TrackManipulation extends Object {
       }
     }
 
+    // construct and return VisADLineArray from geometry
     VisADLineArray array = new VisADLineArray();
     array.vertexCount = nv;
     float[] coordinates = new float[3 * nv];
@@ -297,12 +331,15 @@ public class TrackManipulation extends Object {
     return array;
   }
 
+  /** this CellImpl computes storm track shapes from RealTuples
+      at start and end of track */
   class TrackGetter extends CellImpl {
 
     public TrackGetter() {
     }
 
     public void doAction() throws VisADException, RemoteException {
+      // get start and end locations, first in lat and lon
       float[] latvalues = new float[2];
       float[] lonvalues = new float[2];
       RealTuple tuple1 = (RealTuple) track1_ref.getData();
@@ -317,6 +354,7 @@ public class TrackManipulation extends Object {
         ((Real) tuple2.getComponent(0)).getValue(RealType.Latitude.getDefaultUnit());
       lonvalues[1] = (float)
         ((Real) tuple2.getComponent(1)).getValue(RealType.Longitude.getDefaultUnit());
+      // scale end locations to graphics coordiantes
       latvalues = latmap.scaleValues(latvalues);
       lonvalues = lonmap.scaleValues(lonvalues);
 
@@ -326,15 +364,21 @@ public class TrackManipulation extends Object {
         values[lonindex][k] = lonvalues[k];
         values[otherindex][k] = othervalue;
       }
+      // if necessary, convert to Cartesian graphics coordinates
       if (coord != null) {
         values = coord.toReference(values);
       }
 
+      // disable display so it doesn't update twice
       display.disableAction();
+      // first, base storm track shape at location of first
+      // manipulable RealTuple
       RealTuple track3 = new RealTuple(latlonshape, new double[] {lat0, lon0, 0.0});
       track3_ref.setData(track3);
+      // second, update shape based on end points
       VisADGeometryArray shape = makeTrack(values);
       shapecontrol.setShape(0, shape);
+      // now let display update, just once
       display.enableAction();
     }
 
@@ -345,8 +389,8 @@ public class TrackManipulation extends Object {
       java visad.bom.TrackManipulation xsize ysize angle(degrees) */
   public static void main(String args[])
          throws VisADException, RemoteException {
+    // get command line arguments for ellipse shape
     float[] fargs = {0.2f, 0.1f, 0.0f};
-
     for (int i=0; i<args.length; i++) {
       try {
         fargs[i] = Float.parseFloat(args[i]);
@@ -355,8 +399,7 @@ public class TrackManipulation extends Object {
       }
     }
 
-    // construct Java3D display and mappings that govern
-    // how track records are displayed
+    // construct Java3D display with lat and lon mappings
     DisplayImplJ3D display =
       new DisplayImplJ3D("display1", new TwoDDisplayRendererJ3D());
     ScalarMap lonmap = new ScalarMap(RealType.Longitude, Display.XAxis);
@@ -366,6 +409,8 @@ public class TrackManipulation extends Object {
     display.addMap(latmap);
     latmap.setRange(-10.0, 10.0);
 
+    // construct a TrackManipulation object that sets up manipulation
+    // of storm track shape
     TrackManipulation track = new TrackManipulation(0.0f, 0.0f, 3.0f, 3.0f,
                                        display, fargs[0], fargs[1], fargs[2]);
 
@@ -391,5 +436,4 @@ public class TrackManipulation extends Object {
   }
 
 }
-
 
