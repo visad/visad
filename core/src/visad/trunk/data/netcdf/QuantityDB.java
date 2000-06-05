4120b4eda53ee7cdf0c86fc1f632db017a4a0480
Steve Emmerson <steve@unidata.ucar.edu>
2000-06-05 22:36:35 +0000

    Removed add(String[] definitions, String[] aliases). Added get(String name, String unitSpec). Added add(String name, Quantity quantity). Added add(Quantity quantity).

--- core/src/visad/trunk/data/netcdf/QuantityDB.java
+++ core/src/visad/trunk/data/netcdf/QuantityDB.java
@@ -6,7 +6,7 @@
  * Copyright 1998, University Corporation for Atmospheric Research
  * See file LICENSE for copying and redistribution conditions.
  *
- * $Id: QuantityDB.java,v 1.7 2000-04-26 15:45:14 dglo Exp $
+ * $Id: QuantityDB.java,v 1.8 2000-06-05 22:36:35 steve Exp $
  */
 
 package visad.data.netcdf;
@@ -14,8 +14,11 @@ package visad.data.netcdf;
 import java.util.Iterator;
 import visad.TypeException;
 import visad.Unit;
+import visad.UnitException;
 import visad.VisADException;
+import visad.data.netcdf.units.NoSuchUnitException;
 import visad.data.netcdf.units.ParseException;
+import visad.data.netcdf.units.Parser;
 
 
 /**
@@ -53,26 +56,78 @@ QuantityDB
 
 
     /**
-     * Adds the given quantities and aliases to the database.
+     * Returns the quantity that matches the given name and unit.  If
+     * necessary, it creates the quantity and adds it to the database.
      *
-     * @param definitions	New quantities and their definitions.
-     *				<code>definitions[2*i]</code> contains the
-     *				name (e.g. "speed") of the quantity whose
-     *				preferred unit specification (e.g. "m/s") is
-     *				in <code>definitions[2*i+1]</code>.
-     * @param aliases		Aliases for quantities.  <code>aliases[2*i]
-     *				</code> contains the alias for the quantity
-     *				named in <code>aliases[2*i+1]</code>.
-     * @return			The database resulting from the addition.  May
-     *				or may not be the original object.
-     * @throws ParseException	A unit specification couldn't be parsed.
-     * @throws TypeException	An incompatible version of the quantity already
-     *				exists.
+     * @param name		The name of the quantity.
+     * @param unit		The unit of the quantity.
+     * @return			The quantity in the database that matches
+     *				<code>name</code> and <code>unit</code>.  Note 
+     *				that RETURN_VALUE<code>.getName().equals(name)
+     *				</code> can be <code>false</code> due to
+     *				aliasing and RETURN_VALUE<code>.
+     *				getDefaultUnit().equals(unit)</code> can be 
+     *				<code>false</code> due to allowable unit
+     *				conversion.
+     * @throws ParseException	Couldn't decode <code>unitSpec</code>.
+     * @throws NoSuchUnitException
+     *				<code>unitSpec</code> not in unit database.
+     * @throws UnitException	The quantity already exists with an 
+     *				incompatible unit.
      * @throws VisADException	Couldn't create necessary VisAD object.
      */
-    public abstract QuantityDB
-    add(String[] definitions, String[] aliases)
-	throws ParseException, TypeException, VisADException;
+    public synchronized Quantity
+    get(String name, String unitSpec)
+	throws ParseException, NoSuchUnitException, UnitException,
+	    VisADException
+    {
+	Quantity	quantity = get(name);
+	if (quantity == null)
+	{
+	    quantity = new Quantity(name, unitSpec);
+	    add(quantity);
+	}
+	else
+	{
+	    Unit	quantityUnit = quantity.getDefaultUnit();
+	    if (!Unit.canConvert(Parser.parse(unitSpec), quantityUnit))
+		throw new UnitException(
+		    "Quantity " + name + " already exists; it's unit " +
+		    quantity.getDefaultUnitString() +
+		    " is inconvertible with " + unitSpec);
+	}
+	return quantity;
+    }
+
+
+    /**
+     * Adds a given Quantity to the database under a given name.
+     *
+     * @param name		The name under which the quantity is to be
+     *				added.  May be an alias.
+     * @param quantity		The quantity to be added.
+     * @throws VisADException	Couldn't create necessary VisAD object.
+     */
+    public abstract void
+    add(String name, Quantity quantity)
+	throws VisADException;
+
+
+    /**
+     * Adds a given Quantity to the database.
+     *
+     * @param quantity		The quantity to be added.  The quantity will
+     *				be added under it own name.
+     * @return			The database resulting from the addition.  May
+     *				or may not be the original object.
+     * @throws VisADException	Couldn't create necessary VisAD object.
+     */
+    public void
+    add(Quantity quantity)
+	throws VisADException
+    {
+	add(quantity.getName(), quantity);
+    }
 
 
     /**
