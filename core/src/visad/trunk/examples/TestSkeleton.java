2fd6146558563425e34158fc630d656689b8a132
Dave Glowacki <dglo@ssec.wisc.edu>
2000-08-25 20:47:06 +0000

    Document argument processing methods

--- core/src/visad/trunk/examples/TestSkeleton.java
+++ core/src/visad/trunk/examples/TestSkeleton.java
@@ -54,23 +54,97 @@ public abstract class TestSkeleton
     startThreads();
   }
 
+  boolean hasClientServerMode() { return true; }
+
+  /**
+   * Method used to initialize any instance variables which may be
+   * changed by a cmdline option.<br>
+   * <br>
+   * This is needed because arguments are processed inside the
+   * constructor.  This means that the first line in the constructor
+   * of classes which extend this class will be <tt>super(args)</tt>,
+   * which gets run <em>before</em> any instance variables for that
+   * class are initialized.
+   */
+  void initializeArgs() { }
+
+  /**
+   * Handle subclass-specific command line options and their arguments.<br>
+   * <br>
+   * If <tt>-abc -d efg -h -1 -i</tt> is specified, this
+   * method will be called a maximum of 5 times:<ul>
+   * <li><tt>checkExtraOption(progName, 'a', "bc");</tt>
+   * <li><tt>checkExtraOption(progName, 'd', "efg");</tt>
+   * <li><tt>checkExtraOption(progName, 'h', "-1");</tt>
+   * <li><tt>checkExtraOption(progName, '1', "-i");</tt>
+   * <li><tt>checkExtraOption(progName, 'i', null);</tt>
+   * </ul>
+   * <br>
+   * Note that either of the last two method calls may not
+   * happen if the preceeding method call claims to have used
+   * the following argument (by returning <tt>2</tt>.<br>
+   * <br>
+   * For example,
+   * if the third call (where <tt>ch</tt> is set to <tt>'h'</tt>)
+   * returns <tt>0</tt> or <tt>1</tt>, the next call will contain
+   * <tt>'1'</tt> and <tt>"-i"</tt>.  If, however, the third call
+   * returns <tt>2</tt>, the next call will contain <tt>'i'</tt>
+   * and <tt>null</tt>.
+   *
+   * @param progName The name of the original program (useful for
+   *                 error messages.
+   * @param ch Option character.  If <tt>-a</tt> is specified
+   *           on the command line, <tt>'a'</tt> would be passed to
+   *           this method.)
+   * @param arg The argument associated with this option.
+   *
+   * @return less than <tt>0</tt> to indicate an error<br>
+   *         <tt>0</tt> to indicate that this option is not used by this
+   *         class<br>
+   *         <tt>1</tt> to indicate that only the option was used<br>
+   *         <tt>2</tt> or greater to indicate that both the option and the
+   *         argument were used
+   */
   int checkExtraOption(String progName, char ch, String arg)
   {
     return 0;
   }
 
+  /**
+   * A short string included in the usage message to indicate
+   * valid options.  An example might be <tt>"[-t type]"</tt>.
+   *
+   * @return A <em>very</em> terse description string.
+   */
   String extraOptionUsage() { return ""; }
 
-  int checkExtraKeyword(String progName, int argc, String[] args)
+  /**
+   * Handle subclass-specific command line options and their arguments.
+   *
+   * @param progName The name of the original program (useful for
+   *                 error messages.
+   * @param thisArg The index of the current keyword.
+   * @param args The full list of arguments.
+   *
+   * @return less than <tt>0</tt> to indicate an error<br>
+   *         <tt>0</tt> to indicate that this argument is not used by this
+   *         class<br>
+   *         <tt>1 or more</tt> to indicate the number of arguments used<br>
+   */
+  int checkExtraKeyword(String progName, int thisArg, String[] args)
   {
     return 0;
   }
 
+  /**
+   * A short string included in the usage message to indicate
+   * valid keywords.  An example might be <tt>"[username] [password]"</tt>.
+   *
+   * @return A <em>very</em> terse description string.
+   */
   String extraKeywordUsage() { return ""; }
 
-  boolean hasClientServerMode() { return true; }
-
-  public boolean processArgs(String[] args)
+  boolean processArgs(String[] args)
   {
     boolean usage = false;
 
@@ -82,6 +156,7 @@ public abstract class TestSkeleton
     }
     String progName = className.substring(pt == -1 ? 0 : pt + 1);
 
+    initializeArgs();
     for (int i = 0; args != null && i < args.length; i++) {
       if (args[i].length() > 0 && args[i].charAt(0) == '-') {
         char ch = args[i].charAt(1);
@@ -133,8 +208,12 @@ public abstract class TestSkeleton
 
           int handled = checkExtraOption(progName, ch, str);
           if (handled > 0) {
-            if (strInOption && handled > 1) {
-              handled--;
+            if (handled > 1) {
+              if (strInOption) {
+                handled = 1;
+              } else {
+                handled = 2;
+              }
             }
             i += (handled - 1);
           } else {
