60be13fd8d1143be96cc8aa92da9bc50398ccdbf
Dave Glowacki <dglo@ssec.wisc.edu>
2001-05-02 19:02:16 +0000

    With Don Murray and Steve Emmerson's blessing, convert these to the VisAD copyright

--- core/src/visad/trunk/OffsetUnit.java
+++ core/src/visad/trunk/OffsetUnit.java
@@ -1,14 +1,28 @@
-
 //
 // OffsetUnit.java
 //
 
 /*
- * Copyright 1997, University Corporation for Atmospheric Research
- * See file LICENSE for copying and redistribution conditions.
- *
- * $Id: OffsetUnit.java,v 1.12 2001-03-23 16:56:31 steve Exp $
- */
+VisAD system for interactive analysis and visualization of numerical
+data.  Copyright (C) 1996 - 2001 Bill Hibbard, Curtis Rueden, Tom
+Rink, Dave Glowacki, Steve Emmerson, Tom Whittaker, Don Murray, and
+Tommy Jasmin.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA
+*/
 
 package visad;
 
