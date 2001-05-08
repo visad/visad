41b88aea6fb89f52f31f044ede0895afb53640bf
Bill Hibbard <billh@ssec.wisc.edu>
2001-05-08 17:57:49 +0000

    add non-working adjustSeam() method

--- core/src/visad/trunk/VisADLineArray.java
+++ core/src/visad/trunk/VisADLineArray.java
@@ -81,6 +81,175 @@ public class VisADLineArray extends VisADGeometryArray {
     return array;
   }
 
+  private final static int TEST = 1;
+  private final static float LIMIT = 4.0f; // constant for TEST = 0
+  private final static float ALPHA = 0.1f; // constant for TEST = 1
+
+  public VisADGeometryArray adjustSeam(DataRenderer renderer)
+         throws VisADException {
+// System.out.println("VisADLineArray.adjustSeam");
+    CoordinateSystem coord_sys = renderer.getDisplayCoordinateSystem();
+    // WLH 13 March 2000
+    // if (coord_sys == null) return this;
+    if (coord_sys == null || coord_sys instanceof SphericalCoordinateSystem) {
+      return this;
+    }
+
+    int len = coordinates.length / 3;
+
+    // WLH 15 March 2000
+    if (len < 6) return this;
+
+// System.out.println("VisADLineArray.adjustSeam try");
+
+    float[][] cs = new float[3][len];
+    int j = 0;
+    for (int i=0; i<len; i++) {
+      cs[0][i] = coordinates[j++];
+      cs[1][i] = coordinates[j++];
+      cs[2][i] = coordinates[j++];
+    }
+    float[][] rs = coord_sys.fromReference(cs);
+    boolean[] test = new boolean[len];
+    int last_i;
+
+    if (TEST == 0) {
+      float[] ratios = new float[len];
+      for (int i=0; i<len;  i++) ratios[i] = 0.0f;
+      float mean_ratio = 0.0f;
+      float var_ratio = 0.0f;
+      float max_ratio = 0.0f;
+      int num_ratio = 0;
+
+      for (int i=0; i<len; i+=2) {
+        float cd = (cs[0][i+1] - cs[0][i]) * (cs[0][i+1] - cs[0][i]) +
+                   (cs[1][i+1] - cs[1][i]) * (cs[1][i+1] - cs[1][i]) +
+                   (cs[2][i+1] - cs[2][i]) * (cs[2][i+1] - cs[2][i]);
+        float rd = (rs[0][i+1] - rs[0][i]) * (rs[0][i+1] - rs[0][i]) +
+                   (rs[1][i+1] - rs[1][i]) * (rs[1][i+1] - rs[1][i]) +
+                   (rs[2][i+1] - rs[2][i]) * (rs[2][i+1] - rs[2][i]);
+        if (rd > 0.0f) {
+          ratios[i] = cd / rd;
+          num_ratio++;
+          mean_ratio += ratios[i];
+          var_ratio += ratios[i] * ratios[i];
+          if (ratios[i] > max_ratio) max_ratio = ratios[i];
+        }
+      } // end for (int i=0; i<len; i+=2)
+      if (num_ratio < 2) return this;
+      mean_ratio = mean_ratio / num_ratio;
+      var_ratio = (float)
+        Math.sqrt((var_ratio - mean_ratio * mean_ratio) / num_ratio);
+      float limit_ratio = mean_ratio + LIMIT * var_ratio;
+      if (max_ratio < limit_ratio) return this;
+      for (int i=0; i<len; i+=2) {
+        test[i] = (ratios[i] > limit_ratio);
+      }
+    }
+    else if (TEST == 1) {
+      if (len < 2) return this;
+      float[][] bs = new float[3][len-1];
+      float ALPHA1 = 1.0f + ALPHA;
+      for (int i=0; i<len-1; i++) {
+        bs[0][i] = ALPHA1 * rs[0][i] - ALPHA * rs[0][i+1];
+        bs[1][i] = ALPHA1 * rs[1][i] - ALPHA * rs[1][i+1];
+        bs[2][i] = ALPHA1 * rs[2][i] - ALPHA * rs[2][i+1];
+      }
+      float[][] ds = coord_sys.toReference(bs);
+      float IALPHA = 1.0f / ALPHA;
+      for (int i=0; i<len; i+=2) {
+        float a0 = cs[0][i+1] - cs[0][i];
+        float a1 = cs[1][i+1] - cs[1][i];
+        float a2 = cs[2][i+1] - cs[2][i];
+        float b0 = IALPHA * (cs[0][i] - ds[0][i]);
+        float b1 = IALPHA * (cs[1][i] - ds[1][i]);
+        float b2 = IALPHA * (cs[2][i] - ds[2][i]);
+        float aa = (a0 * a0 + a1 * a1 + a2 * a2);
+        float bb = (b0 * b0 + b1 * b1 + b2 * b2);
+        float ab = (b0 * a0 + b1 * a1 + b2 * a2);
+        // b = A projected onto B, as a signed fraction of B
+        float b = ab / bb;
+        // c = (norm(A projected onto B) / norm(A)) ^ 2
+        float c = (ab * ab) / (aa * bb);
+        test[i] = !(0.5f < b && b < 2.0f && 0.5f < c);
+      } // end for (int i=0; i<len; i+=2)
+    } // end TEST == 1
+    cs = null;
+    rs = null;
+
+    float[] lastcoord = null;
+    byte[] lastcol = null;
+    VisADLineArray array = new VisADLineArray();
+    // worst case splits every line
+    float[] coords = new float[3 * coordinates.length];
+    int color_length = 0;
+    byte[] cols = null;
+    if (colors != null) {
+      color_length = 3;
+      cols = new byte[3 * colors.length];
+      if (colors.length != coordinates.length) color_length = 4;
+    }
+
+    int ki = 0;
+    int kj = 0;
+    j = 0;
+    boolean any_split = false;
+    int accum = 0; // strip counter
+    for (int i=0; i<len; i+=6) {
+      if (!test[i/3]) {
+        coords[ki] = coordinates[i];
+        coords[ki+1] = coordinates[i+1];
+        coords[ki+2] = coordinates[i+2];
+        coords[ki+3] = coordinates[i+3];
+        coords[ki+4] = coordinates[i+4];
+        coords[ki+5] = coordinates[i+5];
+        ki += 6;
+        if (color_length == 3) {
+          cols[kj] = colors[j];
+          cols[kj+1] = colors[j+1];
+          cols[kj+2] = colors[j+2];
+          cols[kj+3] = colors[j+3];
+          cols[kj+4] = colors[j+4];
+          cols[kj+5] = colors[j+5];
+          kj += 6;
+        }
+        else if (color_length == 4) {
+          cols[kj] = colors[j];
+          cols[kj+1] = colors[j+1];
+          cols[kj+2] = colors[j+2];
+          cols[kj+3] = colors[j+3];
+          cols[kj+4] = colors[j+4];
+          cols[kj+5] = colors[j+5];
+          cols[kj+6] = colors[j+6];
+          cols[kj+7] = colors[j+7];
+          kj += 8;
+        }
+      }
+      else {
+        any_split = true;
+      }
+      j += color_length;
+    }
+
+// System.out.println("VisADLineArray.adjustSeam any_split = " + any_split);
+// always coming out false
+
+    if (!any_split) {
+      return this;
+    }
+    else {
+      array.vertexCount = ki / 3;
+      array.coordinates = new float[ki];
+      System.arraycopy(coords, 0, array.coordinates, 0, ki);
+      if (colors != null) {
+        array.colors = new byte[kj];
+        System.arraycopy(cols, 0, array.colors, 0, kj);
+      }
+      return array;
+    }
+  }
+
+
   /**
    * Clone this VisADLineArray
    * @return clone of this
