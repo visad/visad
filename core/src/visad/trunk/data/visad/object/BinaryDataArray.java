c75f7674fe0e9eaf42b4adee8bc7e6e10e0e948c
Dave Glowacki <dglo@ssec.wisc.edu>
2001-07-31 19:14:20 +0000

    Fix debugging msg

--- core/src/visad/trunk/data/visad/object/BinaryDataArray.java
+++ core/src/visad/trunk/data/visad/object/BinaryDataArray.java
@@ -45,7 +45,7 @@ if(DEBUG_WR_DATA_DETAIL)System.err.println("rdDataRA: #" + i + " (" + array[i] +
 
 if(DEBUG_WR_DATA)System.err.println("rdDataRA#"+i+": "+array[i].getClass().getName());
     }
-if(DEBUG_RD_TIME)System.err.println("rdDataRA: "+len+" arrays "+(System.currentTimeMillis()-t));
+if(DEBUG_RD_TIME)System.err.println("rdDataRA: "+len+" elements "+(System.currentTimeMillis()-t));
 
     return array;
   }
