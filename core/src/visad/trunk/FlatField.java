a63aa12fd866468d571bc4592b98d44aff19a38e
Steve Emmerson <steve@unidata.ucar.edu>
2001-09-13 22:15:42 +0000

    Improved JavaDoc for getFloats(), getFloats(boolean), getValues(), and getValues(boolean).

--- core/src/visad/trunk/FlatField.java
+++ core/src/visad/trunk/FlatField.java
@@ -1292,28 +1292,22 @@ public class FlatField extends FieldImpl implements FlatFieldIface {
     return range;
   }
 
+  /**
+   * Returns the range values in their default units as floats.
+   *
+   * @return			The range values in their default units
+   *				as determined by the {@link MathType} of
+   *				the range.  Element <code>[i][j]</code> is
+   *				the <code>j</code>th sample value of the
+   *				<code>i</code>th component of the range.
+   * @throws VisADException     if a VisAD object couldn't be created.
+   */
   public float[][] getFloats() throws VisADException {
     return getFloats(true);
   }
 
-  /** get this FlatField's range values in their default range
-      Units (as defined by the range of the FlatField's
-      FunctionType); the return array is dimensioned
-      float[number_of_range_components][number_of_range_samples];
-      return a copy if copy == true */
-  public float[][] getFloats(boolean copy) throws VisADException {
-    float[][] values = unpackFloats(copy);
-    Unit[] units_out =
-      ((FunctionType) Type).getFlatRange().getDefaultUnits();
-    return Unit.convertTuple(values, RangeUnits, units_out);
-  }
-
-  public double[][] getValues() throws VisADException {
-    return getValues(true);
-  }
-
   /**
-   * Returns the range values in their default units.
+   * Returns the range values in their default units as floats.
    *
    * @param copy		Whether or not the returned array might be the
    *				actual range array.  If <code>true</code>, then
@@ -1324,6 +1318,42 @@ public class FlatField extends FieldImpl implements FlatFieldIface {
    *				the range.  Element <code>[i][j]</code> is
    *				the <code>j</code>th sample value of the
    *				<code>i</code>th component of the range.
+   * @throws VisADException     if a VisAD object couldn't be created.
+   */
+  public float[][] getFloats(boolean copy) throws VisADException {
+    float[][] values = unpackFloats(copy);
+    Unit[] units_out =
+      ((FunctionType) Type).getFlatRange().getDefaultUnits();
+    return Unit.convertTuple(values, RangeUnits, units_out);
+  }
+
+  /**
+   * Returns the range values in their default units as doubles.
+   *
+   * @return			The range values in their default units
+   *				as determined by the {@link MathType} of
+   *				the range.  Element <code>[i][j]</code> is
+   *				the <code>j</code>th sample value of the
+   *				<code>i</code>th component of the range.
+   * @throws VisADException     if a VisAD object couldn't be created.
+   */
+  public double[][] getValues() throws VisADException {
+    return getValues(true);
+  }
+
+  /**
+   * Returns the range values in their default units as doubles.
+   *
+   * @param copy		Whether or not the returned array might be the
+   *				actual range array.  If <code>true</code>, then
+   *				the returned array will not be the actual range
+   *				array.
+   * @return			The range values in their default units
+   *				as determined by the {@link MathType} of
+   *				the range.  Element <code>[i][j]</code> is
+   *				the <code>j</code>th sample value of the
+   *				<code>i</code>th component of the range.
+   * @throws VisADException     if a VisAD object couldn't be created.
    */
   public double[][] getValues(boolean copy) throws VisADException {
     double[][] values = unpackValues(copy);
