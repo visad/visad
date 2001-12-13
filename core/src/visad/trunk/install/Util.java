cf2e2f6c698a42fa3ca711382f7d8af49db21fff
Dave Glowacki <dglo@ssec.wisc.edu>
2001-12-13 22:14:54 +0000

    Tweak copyFile() a bit Add copyJar()

--- core/src/visad/trunk/install/Util.java
+++ core/src/visad/trunk/install/Util.java
@@ -4,6 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Date;
+import java.util.Enumeration;
+
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Util
 {
@@ -34,7 +41,7 @@ public class Util
   }
 
   /**
-   * Copy files under <i>source</i> directory to <i>target</i>
+   * Copy files under the <i>source</i> directory to the <i>target</i>
    * directory.  If necessary, <i>target</i> directory is created.<br>
    * <br>
    * For example, if this method is called with a <i>source</i> of
@@ -125,13 +132,15 @@ public class Util
   }
 
   /**
-   * Copy <i>source</i> file to <i>target</i> file.
-   * <i>target</i> cannot be a directory.
-   * <br>
+   * Copy the <i>source</i> file to <i>target</i>.  If <i>target</i>
+   * does not exist, it is assumed to be the name of the copied file.
+   * If <i>target</i> is a directory, the file will be copied
+   * into that directory.
+   *
    * @param progess if non-null, this progress monitor is updated
    *                with the name of each file as it is copied.
    * @param source source directory
-   * @param target directory
+   * @param target target file/directory
    * @param saveSuffix if non-null and <i>target</i> exists,
    *                   <i>target</i> will be renamed to
    *                   <tt>name + saveSuffix</tt>.
@@ -143,7 +152,7 @@ public class Util
                                        String saveSuffix)
   {
     // don't copy directories
-    if (source.isDirectory() || target.isDirectory()) {
+    if (source.isDirectory()) {
       return false;
     }
 
@@ -152,6 +161,109 @@ public class Util
       return false;
     }
 
+    if (target.isDirectory()) {
+      target = new File(target, source.getName());
+    }
+
+    FileInputStream  in;
+    try {
+      in = new FileInputStream(source);
+    } catch (IOException ioe) {
+      System.err.println("Couldn't open source file " + source);
+      return false;
+    }
+
+    copyStreamToFile(progress, in, target, saveSuffix);
+
+    try { in.close(); } catch (Exception e) { ; }
+
+    // if source was read-only, the target should be as well
+    if (!source.canWrite()) {
+      target.setReadOnly();
+    }
+
+    // sync up last-modified time
+    target.setLastModified(source.lastModified());
+
+    return true;
+  }
+
+  /**
+   * Copy files in the <i>source</i> jar file to the <i>target</i>
+   * directory.  If necessary, the <i>target</i> directory is created.<br>
+   * <br>
+   * For example, if this method is called with a <i>source</i> of
+   * <tt>foo.jar</tt> (which contains <tt>a</tt> and <tt>b</tt>)
+   * and a <i>target</i> of <tt>/bar</tt>, when this method exits
+   * <tt>/bar</tt> will contain <tt>/bar/a</tt> and <tt>/bar/b</tt>.
+   *
+   * @param progess if non-null, this progress monitor is updated
+   *                with the name of each file as it is copied.
+   * @param source source jar file
+   * @param target directory
+   * @param saveSuffix if non-null, pre-existing files in <i>target</i>
+   *                   whose paths match files to be copied from
+   *                   <i>source</i> will be renamed to
+   *                   <tt>name + saveSuffix</tt>.
+   *
+   * @return false if any problems were encountered.
+   */
+  public static final boolean copyJar(ProgressMonitor progress,
+                                      File source, File target,
+                                      String saveSuffix)
+  {
+    // if target exists, it must be a directory
+    if (target.exists() && !target.isDirectory()) {
+      return false;
+    }
+
+    // if the target doesn't exist yet, create it
+    if (!target.exists()) {
+      target.mkdirs();
+    }
+
+    // try to open the jar file
+    JarFile jar;
+    try {
+      jar = new JarFile(source);
+    } catch (IOException ioe) {
+      return false;
+    }
+
+    boolean result = true;
+
+    Enumeration enum = jar.entries();
+    while (enum.hasMoreElements()) {
+      JarEntry entry = (JarEntry )enum.nextElement();
+
+      File newFile = new File(target, entry.getName());
+      newFile.mkdirs();
+
+      if (!entry.isDirectory()) {
+
+        InputStream in;
+        try {
+          in = jar.getInputStream(entry);
+        } catch (IOException ioe) {
+          System.err.println("Couldn't copy entry " + entry.getName());
+          continue;
+        }
+
+        copyStreamToFile(progress, in, newFile, saveSuffix);
+
+        try { in.close(); } catch (Exception e) { ; }
+      }
+
+      newFile.setLastModified(entry.getTime());
+    }
+
+    return result;
+  }
+
+  private static final boolean copyStreamToFile(ProgressMonitor progress,
+                                                InputStream in, File target,
+                                                String saveSuffix)
+  {
     // if the target already exists and we need to save the existing file...
     if (target.exists()) {
       if (saveSuffix == null) {
@@ -182,14 +294,6 @@ public class Util
       progress.setDetail("Installing " + target);
     }
 
-    FileInputStream  in;
-    try {
-      in = new FileInputStream(source);
-    } catch (IOException ioe) {
-      System.err.println("Couldn't open source file " + source);
-      return false;
-    }
-
     FileOutputStream out;
     try {
       out = new FileOutputStream(target);
@@ -198,10 +302,7 @@ public class Util
       return false;
     }
 
-    final long fileLength = source.length();
-
     byte buffer[]  = new byte[1024];
-
     try {
       long totalBytes = 0;
       while (true) {
@@ -217,17 +318,8 @@ public class Util
       ioe.printStackTrace();
       return false;
     } finally {
-      try { in.close(); } catch (Exception e) { ; }
       try { out.close(); } catch (Exception e) { ; }
     }
-
-    // if source was read-only, the target should be as well
-    if (!source.canWrite()) {
-      target.setReadOnly();
-    }
-
-    // sync up last-modified time
-    target.setLastModified(source.lastModified());
 
     return true;
   }
