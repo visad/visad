a775350d4f041845feec71769919af1c338b3337
Dave Glowacki <dglo@ssec.wisc.edu>
2001-12-13 16:49:38 +0000

    Alphabetize

--- core/src/visad/trunk/install/Util.java
+++ core/src/visad/trunk/install/Util.java
@@ -7,13 +7,66 @@ import java.io.IOException;
 
 public class Util
 {
-  public static final String getPath(File f)
+  public static final boolean copyDirectory(File source, File target)
   {
-    try {
-      return f.getCanonicalPath();
-    } catch (IOException ioe) {
-      return f.getAbsolutePath();
+    return copyDirectory(null, source, target, null);
+  }
+
+  public static final boolean copyDirectory(ProgressMonitor progress,
+                                            File source, File target)
+  {
+    return copyDirectory(progress, source, target, null);
+  }
+
+  public static final boolean copyDirectory(File source, File target,
+                                            String saveSuffix)
+  {
+    return copyDirectory(null, source, target, saveSuffix);
+  }
+
+  public static final boolean copyDirectory(ProgressMonitor progress,
+                                            File source, File target,
+                                            String saveSuffix)
+  {
+    // source must be a directory
+    if (!source.isDirectory() || (target.exists() && !target.isDirectory())) {
+      return false;
     }
+
+    // if source and target are the same, we're done
+    // if source and target are the same, we're done
+    if (getPath(source).equals(getPath(target))) {
+      return false;
+    }
+
+    // if the target doesn't exist yet, create it
+    if (!target.exists()) {
+      target.mkdirs();
+    }
+
+    boolean result = true;
+
+    String[] list = source.list();
+    for (int i = 0; i < list.length; i++) {
+      File srcFile = new File(source, list[i]);
+      File tgtFile = new File(target, list[i]);
+
+      if (srcFile.isDirectory()) {
+        result |= copyDirectory(progress, srcFile, tgtFile, saveSuffix);
+      } else {
+        result |= copyFile(progress, srcFile, tgtFile, saveSuffix);
+      }
+    }
+
+    // if source was read-only, the target should be as well
+    if (!source.canWrite()) {
+      target.setReadOnly();
+    }
+
+    // sync up last-modified time
+    target.setLastModified(source.lastModified());
+
+    return result;
   }
 
   public static final boolean copyFile(File source, File target)
@@ -127,65 +180,12 @@ public class Util
     return true;
   }
 
-  public static final boolean copyDirectory(File source, File target)
+  public static final String getPath(File f)
   {
-    return copyDirectory(null, source, target, null);
-  }
-
-  public static final boolean copyDirectory(ProgressMonitor progress,
-                                            File source, File target)
-  {
-    return copyDirectory(progress, source, target, null);
-  }
-
-  public static final boolean copyDirectory(File source, File target,
-                                            String saveSuffix)
-  {
-    return copyDirectory(null, source, target, saveSuffix);
-  }
-
-  public static final boolean copyDirectory(ProgressMonitor progress,
-                                            File source, File target,
-                                            String saveSuffix)
-  {
-    // source must be a directory
-    if (!source.isDirectory() || (target.exists() && !target.isDirectory())) {
-      return false;
+    try {
+      return f.getCanonicalPath();
+    } catch (IOException ioe) {
+      return f.getAbsolutePath();
     }
-
-    // if source and target are the same, we're done
-    // if source and target are the same, we're done
-    if (getPath(source).equals(getPath(target))) {
-      return false;
-    }
-
-    // if the target doesn't exist yet, create it
-    if (!target.exists()) {
-      target.mkdirs();
-    }
-
-    boolean result = true;
-
-    String[] list = source.list();
-    for (int i = 0; i < list.length; i++) {
-      File srcFile = new File(source, list[i]);
-      File tgtFile = new File(target, list[i]);
-
-      if (srcFile.isDirectory()) {
-        result |= copyDirectory(progress, srcFile, tgtFile, saveSuffix);
-      } else {
-        result |= copyFile(progress, srcFile, tgtFile, saveSuffix);
-      }
-    }
-
-    // if source was read-only, the target should be as well
-    if (!source.canWrite()) {
-      target.setReadOnly();
-    }
-
-    // sync up last-modified time
-    target.setLastModified(source.lastModified());
-
-    return result;
   }
 }
