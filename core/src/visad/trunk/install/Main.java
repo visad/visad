f11ed0e7a98d515f760ecf0cb7745f517a616feb
Dave Glowacki <dglo@ssec.wisc.edu>
2001-12-18 18:06:18 +0000

    Merge trivial ClusterInstaller class into Main

--- core/src/visad/trunk/install/Main.java
+++ core/src/visad/trunk/install/Main.java
@@ -19,6 +19,42 @@ import javax.swing.JWindow;
 import visad.util.CmdlineGenericConsumer;
 import visad.util.CmdlineParser;
 
+class ClusterInstaller
+{
+  private Runtime runtime = null;
+  private String[] argList = null;
+
+  private String cPush;
+
+  public ClusterInstaller(String cPush)
+  {
+    this.cPush = cPush;
+  }
+
+  public Process push(String target)
+    throws IOException
+  {
+    return push(target, target);
+  }
+
+  public Process push(String source, String target)
+    throws IOException
+  {
+    if (runtime == null) {
+      runtime = Runtime.getRuntime();
+    }
+
+    if (argList == null) {
+      argList = new String[] { cPush, null, null };
+    }
+
+    argList[1] = source;
+    argList[2] = target;
+
+    return runtime.exec(argList);
+  }
+}
+
 public class Main
   extends CmdlineGenericConsumer
 {
