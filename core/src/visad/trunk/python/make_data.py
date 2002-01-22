1b03f2c54e8c641b4b3805721df1ff5722ba9490
Bill Hibbard <billh@ssec.wisc.edu>
2002-01-22 20:24:45 +0000

    create four Real files

--- core/src/visad/trunk/python/make_data.py
+++ core/src/visad/trunk/python/make_data.py
@@ -11,15 +11,21 @@ file.save("generic_real.nc", a, 1)
 
 # create typed real file
 t = RealType("temp")
-rt = Real(t, 1.0)
-file.save("real.nc", rt, 1)
+rt0 = Real(t, 0.0)
+file.save("real0.nc", rt0, 1)
+rt1 = Real(t, 1.0)
+file.save("real1.nc", rt1, 1)
+rt2 = Real(t, 2.0)
+file.save("real2.nc", rt2, 1)
+rt3 = Real(t, 3.0)
+file.save("real3.nc", rt3, 1)
 
 # create typed real vector file
 p = RealType("pres")
 h = RealType("hum")
 rp = Real(p, 2.0)
 rh = Real(h, -1.0)
-reals = [rt, rp, rh]
+reals = [rt1, rp, rh]
 v = RealTuple(reals)
 file.save("vector.nc", v, 1)
 
