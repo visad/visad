127987a466da01765651e93fce0fa707a27d8e45
Curtis Rueden <ctrueden@wisc.edu>
2002-02-12 23:00:35 +0000

    color range mapping widget

---
+++ core/src/visad/trunk/bio/BioColorMapWidget.java
@@ -0,0 +1,124 @@
+//
+// BioColorMapWidget.java
+//
+
+/*
+VisAD system for interactive analysis and visualization of numerical
+data.  Copyright (C) 1996 - 2002 Bill Hibbard, Curtis Rueden, Tom
+Rink, Dave Glowacki, Steve Emmerson, Tom Whittaker, Don Murray, and
+Tommy Jasmin.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA
+*/
+
+package visad.bio;
+
+import java.awt.Dimension;
+import java.awt.event.*;
+import javax.swing.*;
+import visad.*;
+
+/**
+ * BioColorMapWidget is a widget for controlling mappings
+ * from range scalars to color scalars.
+ */
+public class BioColorMapWidget extends JPanel {
+
+  // -- CONSTANTS --
+
+  public static final int RED = 0;
+  public static final int GREEN = 1;
+  public static final int BLUE = 2;
+  public static final int RGB = 3;
+
+  public static final int CYAN = 4;
+  public static final int MAGENTA = 5;
+  public static final int YELLOW = 6;
+  public static final int CMY = 7;
+
+  public static final int HUE = 8;
+  public static final int SATURATION = 9;
+  public static final int VALUE = 10;
+  public static final int HSV = 11;
+
+  private static final DisplayRealType[] COLOR_TYPES = {
+    Display.Red, Display.Green, Display.RGB,
+    Display.Cyan, Display.Magenta, Display.Yellow, Display.CMY,
+    Display.Hue, Display.Saturation, Display.Value, Display.HSV
+  };
+
+  private static final String[] COLOR_NAMES = {
+    "Red", "Green", "Blue", "RGB",
+    "Cyan", "Magenta", "Yellow", "CMY",
+    "Hue", "Saturation", "Value", "HSV"
+  };
+
+
+  // -- GUI COMPONENTS --
+
+  private JLabel color;
+  private JComboBox scalars;
+
+
+  // -- OTHER FIELDS --
+
+  private BioVisAD bio;
+  private DisplayRealType type;
+  private ScalarMap map;
+
+
+  // -- CONSTRUCTOR --
+
+  /** Constructs a new animation widget. */
+  public BioColorMapWidget(BioVisAD biovis, int colorType) {
+    bio = biovis;
+    type = COLOR_TYPES[colorType];
+    color = new JLabel(COLOR_NAMES[colorType] + ":") {
+      public Dimension getPreferredSize() {
+        Dimension d = super.getPreferredSize();
+        return new Dimension(50, d.height);
+      }
+    };
+    scalars = new JComboBox();
+    scalars.addItem("None");
+    scalars.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        bio.sm.reconfigureDisplay();
+      }
+    });
+    setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+    add(color);
+    add(scalars);
+  }
+
+
+  // -- API METHODS --
+
+  /** Enables or disables this widget. */
+  public void setEnabled(boolean enabled) {
+    color.setEnabled(enabled);
+    scalars.setEnabled(enabled);
+  }
+
+  /** Refreshes the combo box to contain the current range types. */
+  public void refreshTypes() {
+    for (int i=0; i<bio.sm.rtypes.length; i++) {
+      scalars.addItem(bio.sm.rtypes[i]);
+    }
+    // CTR - TODO - autodetect types - huh?
+  }
+
+}
