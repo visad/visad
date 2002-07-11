c66eed45d3f62cffb8db789a530dd3cffe1b2e4d
Bill Hibbard <billh@ssec.wisc.edu>
2002-07-11 11:09:03 +0000

    move Java3D import out of visad package

--- core/src/visad/trunk/Stream2D.java
+++ core/src/visad/trunk/Stream2D.java
@@ -26,8 +26,6 @@ MA 02111-1307, USA
 
 package visad;
 
-import visad.*;
-import visad.java3d.DisplayImplJ3D;
 import java.rmi.RemoteException;
 
 import javax.swing.JFrame;
@@ -278,7 +276,7 @@ static void stream_trace( float[] ugrid, float[] vgrid, int nr, int nc,
  *          0 = error  (out of memory)
  */
 
-static int stream( float[] ugrid, float[] vgrid, int nr, int nc,
+public static int stream( float[] ugrid, float[] vgrid, int nr, int nc,
                    float density, float stepFactor, float arrowScale,
                    float[][][] vr, float[][][] vc,
                    int[][] numv, int[] numl,
@@ -500,110 +498,5 @@ static int stream( float[] ugrid, float[] vgrid, int nr, int nc,
   return 1;
 }
 
-public static void main(String[] args)
-       throws VisADException, RemoteException
-{
-  int nr = 50;
-  int nc = 50;
-
-  DisplayImpl dpy = new DisplayImplJ3D("display");
-
-  RealType u_wind = RealType.getRealType("u_wind");
-  RealType v_wind = RealType.getRealType("v_wind");
-
-  RealTupleType uv = new RealTupleType(u_wind, v_wind);
-
-
-  FunctionType f_type =
-    new FunctionType(RealTupleType.SpatialCartesian2DTuple, uv);
-
-  Integer2DSet d_set =
-    new Integer2DSet(RealTupleType.SpatialCartesian2DTuple, nr, nc);
-
-  FlatField uv_field =
-    new FlatField(f_type, d_set);
-
-  float[][] uv_values = new float[2][nr*nc];
-  
-  double ang = 2*Math.PI/nr;
-
-  for ( int jj = 0; jj < nc; jj++ ) {
-    for ( int ii = 0; ii < nr; ii++ ) {
-      int idx = jj*nr + ii;
-      uv_values[0][idx] = 5 + -20f*((float) Math.cos(0.5*ang*ii));
-      uv_values[1][idx] = -10f*((float) Math.cos(0.5*ang*ii));
-    }
-  }
-
-  uv_field.setSamples(uv_values, false);
-                                      
-
-  int[] numl = new int[1];
-  int maxv = 1000;
-  int max_lines = 100;
-  int[][] n_verts = new int[1][];
-  float[][][] vr = new float[1][max_lines][maxv];
-  float[][][] vc = new float[1][max_lines][maxv];
-
-
-  stream(uv_values[0], uv_values[1], nr, nc, 1f, 1, 1f, vr, vc, n_verts, numl, null);
-
-  
-  ScalarMap xmap = new ScalarMap(RealType.XAxis, Display.XAxis);
-  ScalarMap ymap = new ScalarMap(RealType.YAxis, Display.YAxis);
-  dpy.addMap(xmap);
-  dpy.addMap(ymap);
-
-  ScalarMap flowx = new ScalarMap(u_wind, Display.Flow1X);
-  ScalarMap flowy = new ScalarMap(v_wind, Display.Flow1Y);
-  dpy.addMap(flowx);
-  dpy.addMap(flowy);
-
-  FlowControl flow_cntrl = (FlowControl) flowx.getControl();
-  flow_cntrl.setFlowScale(0.04f);
-
-  flow_cntrl = (FlowControl) flowy.getControl();
-  flow_cntrl.setFlowScale(0.04f);
-
-  DataReferenceImpl ref = new DataReferenceImpl("wind");
-  ref.setData(uv_field);
-
-  dpy.addReference(ref);
-
-
-  Gridded2DSet[] gsets = new Gridded2DSet[numl[0]];
-  for ( int s_idx = 0; s_idx < numl[0]; s_idx++ ) {
-    float[][] strm_values = new float[2][n_verts[0][s_idx]];
-    System.arraycopy(vc[0][s_idx], 0, strm_values[0], 0, n_verts[0][s_idx]);
-    System.arraycopy(vr[0][s_idx], 0, strm_values[1], 0, n_verts[0][s_idx]);
-
-    gsets[s_idx] =
-      new Gridded2DSet(RealTupleType.SpatialCartesian2DTuple, strm_values, n_verts[0][s_idx]);
-  }
-
-  UnionSet uset = new UnionSet(gsets);
-  DataReferenceImpl strm_ref = new DataReferenceImpl("stream");
-  strm_ref.setData(uset);
-
-  ConstantMap[] strm_cm =
-    new ConstantMap[]
-  {
-    new ConstantMap(0.1, Display.Red),
-    new ConstantMap(0.8, Display.Green),
-    new ConstantMap(0.1, Display.Blue),
-    new ConstantMap(1.5, Display.LineWidth)
-  };
-
-  dpy.addReference(strm_ref, strm_cm);
-
-
-  JFrame jframe  = new JFrame();
-  jframe.addWindowListener(new WindowAdapter() {
-    public void windowClosing(WindowEvent e) {System.exit(0);}
-  });
-
-  jframe.setContentPane((JPanel) dpy.getComponent());
-  jframe.setVisible(true);
 }
 
-}
