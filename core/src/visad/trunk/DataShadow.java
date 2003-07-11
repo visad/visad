a2ee8bdc54461e1d8f1289cc086c7cf6b19523d0
Bill Hibbard <billh@ssec.wisc.edu>
2003-07-11 14:48:09 +0000

    improve JavaDoc

--- core/src/visad/trunk/DataShadow.java
+++ core/src/visad/trunk/DataShadow.java
@@ -30,37 +30,50 @@ import java.util.*;
 import java.rmi.*;
 
 /**
-   DataShadow is the VIsAD class for gathering RealType value
-   ranges, Animation sampling Sets, etc for Data display.<P>
+   DataShadow is the VisAD class for gathering RealType value
+   ranges and Animation sampling Sets for auto-scaling Displays.<P>
 */
 public class DataShadow extends Object implements java.io.Serializable {
 
-  double[][] ranges; // [2][num_RealTypes] - 0=min, 1=max
+  /** ranges of RealType values, dimensioned [2][num_RealTypes] where
+      first index = 0 for min, = 1 for max */
+  double[][] ranges;
 
-  /** default Set for Animation sampling;
-      order of precedence is:
-      1. animationSampling
-      2. animationRangeSampling
-      3. new Linear1DSet(type, low_range, hi_range, 100) */
-  Set animationSampling; // from a Field domain in data
-  Set animationRangeSampling; // from a range value in data (should never happen)
+  /** default Set for Animation sampling, from Field domains in data */
+  Set animationSampling;
 
+  /**
+   * construct a new DataShadow with given ranges array and
+   * null animationSampling
+   * @param r ranges array
+   */
   DataShadow(double[][] r) {
     ranges = r;
     animationSampling = null;
-    animationRangeSampling = null;
   }
 
+  /**
+   * @param domain flag indicating if this call is for Field domain
+   * @return flag indicating that animationSampling is non-null
+   *         and this call is not for a Field domain
+   */
   boolean isAnimationSampling(boolean domain) {
     if (domain) {
       return false;
-      // for first set: return (animationSampling != null);
     }
     else {
-      return (animationSampling != null || animationRangeSampling != null);
+      return (animationSampling != null);
     }
   }
 
+  /**
+   * set or merge a Set into animationSampling, as long as Set is
+   * neither FloatSet nor DoubleSet, and this call is for a Field
+   * domain
+   * @param set Set to merge
+   * @param domain flag indicating if this call is for Field domain
+   * @throws VisADException a VisAD error occurred
+   */
   void setAnimationSampling(Set set, boolean domain)
        throws VisADException {
     if (set instanceof DoubleSet || set instanceof FloatSet) return;
@@ -70,22 +83,24 @@ public class DataShadow extends Object implements java.io.Serializable {
       }
       else {
         animationSampling = animationSampling.merge1DSets(set);
-        // for first set: return;
-      }
-    }
-    else {
-      if (animationSampling == null && animationRangeSampling == null) {
-        animationRangeSampling = set;
       }
     }
   }
 
-  public void merge(DataShadow shadow)
-         throws VisADException {
+  /**
+   * merge argument DataShadow into this DataShadow
+   * @param shadow DataShadow to merge
+   * @throws VisADException a VisAD error occurred
+   */
+  public void merge(DataShadow shadow) throws VisADException {
     int n = ranges[0].length;
     for (int i=0; i<n; i++) {
-      if (shadow.ranges[0][i] < ranges[0][i]) ranges[0][i] = shadow.ranges[0][i];
-      if (shadow.ranges[1][i] > ranges[1][i]) ranges[1][i] = shadow.ranges[1][i];
+      if (shadow.ranges[0][i] < ranges[0][i]) {
+        ranges[0][i] = shadow.ranges[0][i];
+      }
+      if (shadow.ranges[1][i] > ranges[1][i]) {
+        ranges[1][i] = shadow.ranges[1][i];
+      }
     }
     setAnimationSampling(shadow.animationSampling, true);
   }
