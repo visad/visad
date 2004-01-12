86df3fb6ae2856436028b1ade4692f8a16dc4c34
Curtis Rueden <ctrueden@wisc.edu>
2004-01-12 16:16:35 +0000

    Convert old Bio-Rad form classes to stubs and deprecate them.

--- core/src/visad/trunk/data/biorad/BioRadCachingForm.java
+++ core/src/visad/trunk/data/biorad/BioRadCachingForm.java
@@ -26,90 +26,10 @@ MA 02111-1307, USA
 
 package visad.data.biorad;
 
-import java.io.IOException;
-
-import java.net.URL;
-
-import java.rmi.RemoteException;
-
-import ucar.netcdf.RandomAccessFile;
-
-import visad.Data;
-import visad.DataImpl;
-import visad.data.DefaultFamily;
-import visad.VisADException;
-
-import visad.data.BadFormException;
-
 /**
  * BioRadCachingForm is the VisAD data format adapter for
  * large Bio-Rad .PIC files which may not fit in memory.
+ *
+ * @deprecated BioRadCachingForm has moved to the visad.data.bio package.
  */
-public class BioRadCachingForm extends BioRadForm {
-  public BioRadCachingForm()
-  {
-    super();
-  }
-
-  /**
-   * Opens an existing large BioRad .PIC file from the given
-   * location, caching the data where possible.
-   *
-   * @return VisAD Data object containing BioRad data.
-   */
-  public DataImpl open(String id)
-    throws BadFormException, IOException, VisADException
-  {
-    return readFile(new RandomAccessFile(id, "r"), true);
-  }                       
-
-  /**
-   * URLs cannot be cached, so this method throws a
-   * {@link visad.VisADException VisADException}
-   *
-   * @exception VisADException because URLs cannot be cached.
-   */
-  public DataImpl open(URL url)
-    throws BadFormException, VisADException, IOException
-  {
-    throw new VisADException("Cannot cache URL " + url);
-  }
-
-  /**
-   * Run 'java visad.data.biorad.BioRadCachingForm in_file out_file'
-   * to convert in_file to out_file in BioRad .PIC data format.
-   */
-  public static void main(String[] args)
-    throws VisADException, RemoteException, IOException
-  {
-    if (args == null || args.length < 1 || args.length > 2) {
-      System.out.println("To convert a file to BioRad .PIC, run:");
-      System.out.println(
-        "  java visad.data.biorad.BioRadCachingForm in_file out_file");
-      System.out.println("To test read a BioRad .PIC file, run:");
-      System.out.println("  java visad.data.biorad.BioRadCachingForm in_file");
-      System.exit(2);
-    }
-
-    if (args.length == 1) {
-      // Test read BioRad .PIC file
-      BioRadCachingForm form = new BioRadCachingForm();
-      System.out.print("Reading " + args[0] + " ");
-      Data data = form.open(args[0]);
-      System.out.println("[done]");
-      System.out.println("MathType =\n" + data.getType().prettyString());
-    }
-    else if (args.length == 2) {
-      // Convert file to BioRad .PIC format
-      System.out.print(args[0] + " -> " + args[1] + " ");
-      DefaultFamily loader = new DefaultFamily("loader");
-      DataImpl data = loader.open(args[0]);
-      loader = null;
-      BioRadCachingForm form = new BioRadCachingForm();
-      form.save(args[1], data, true);
-      System.out.println("[done]");
-    }
-
-    System.exit(0);
-  }
-}
+public class BioRadCachingForm extends visad.data.bio.BioRadCachingForm { }
