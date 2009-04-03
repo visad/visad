fde691fb8c07cf1238b945bbb13b7da7821db983
Tom Rink <rink@ssec.wisc.edu>
2009-04-03 14:40:45 +0000

    couple of minor changes

--- core/src/visad/trunk/test/ImageAnimationTest.java
+++ core/src/visad/trunk/test/ImageAnimationTest.java
@@ -8,6 +8,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import visad.AnimationControl;
+import visad.CoordinateSystem;
+import visad.GridCoordinateSystem;
 import visad.DataReference;
 import visad.DataReferenceImpl;
 import visad.Display;
@@ -18,6 +20,7 @@ import visad.FlatField;
 import visad.FunctionType;
 import visad.Integer1DSet;
 import visad.Linear2DSet;
+import visad.Gridded2DSet;
 import visad.RealTupleType;
 import visad.RealType;
 import visad.ScalarMap;
@@ -31,6 +34,8 @@ import visad.util.Util;
  * Simple test for <code>AnimationControl</code> and <code>ImageRendererJ3D</code>.
  */
 public class ImageAnimationTest extends JPanel {
+
+  ImageRendererJ3D imgRend;
 
   public static final float[] makeSinusoidalSamples(int lenX, int lenY, int waveNum) {
     float[] samples = new float[lenX*lenY];
@@ -57,41 +62,53 @@ public class ImageAnimationTest extends JPanel {
   public ImageAnimationTest(int num, int size) throws VisADException, RemoteException {
     
     // (Time -> ((x, y) -> val))
-    RealTupleType pxl = new RealTupleType(RealType.getRealType("x"), RealType.getRealType("y"));
+    RealTupleType pxlA = new RealTupleType(RealType.getRealType("x_a"), RealType.getRealType("y_a"));
+    //-Linear2DSet imgSet = new Linear2DSet(pxl, 0, size-1, size, 0, size-1, size); 
+    Linear2DSet set = new Linear2DSet(RealTupleType.Generic2D, 0, size-1, size, 0, size-1, size); 
+    float[][] locs = set.getSamples();
+    Gridded2DSet locs_set = new Gridded2DSet(pxlA, locs, size, size); 
+    GridCoordinateSystem gcs = new GridCoordinateSystem(locs_set);
+
+
+    //-RealTupleType pxl = new RealTupleType(RealType.getRealType("x"), RealType.getRealType("y"));
+    RealTupleType pxl = new RealTupleType(RealType.getRealType("x"), RealType.getRealType("y"), gcs, null);
+    Linear2DSet imgSet = new Linear2DSet(pxl, 0, size-1, size, 0, size-1, size); 
     FunctionType pxlVal = new FunctionType(pxl, RealType.getRealType("val"));
     FunctionType timePxlVal = new FunctionType(RealType.Time, pxlVal);
-    System.err.println(timePxlVal.toString());
     
+
+
     Field timeFld = new FieldImpl(timePxlVal, new Integer1DSet(RealType.Time, num));
     
     float[][] samples = makeSamples(size, num);
     
     for (int i = 0; i < samples.length; i++) {
-      Linear2DSet imgSet = new Linear2DSet(pxlVal.getDomain(), 
-          0, size - 1, size, 0, size - 1, size);
       FlatField imgFld = new FlatField(pxlVal, imgSet);
 
-      imgFld.setSamples(new float[][]{samples[i]});
+      imgFld.setSamples(new float[][]{samples[i]}, false);
       
       // set image for time
-      timeFld.setSample(i, imgFld);
+      timeFld.setSample(i, imgFld, false);
     }
+    System.out.println("makeSamples done");
     
     DataReference ref = new DataReferenceImpl("image");
     ref.setData(timeFld);
     
     DisplayImpl display = new DisplayImplJ3D("image display");
-    ImageRendererJ3D imgRend = new ImageRendererJ3D();
+    imgRend = new ImageRendererJ3D();
     System.err.println("Renderer:"+imgRend.toString());
 
-    display.addMap(new ScalarMap(RealType.getRealType("x"), Display.XAxis));
-    display.addMap(new ScalarMap(RealType.getRealType("y"), Display.YAxis));
-    display.addMap(new ScalarMap(RealType.getRealType("val"), Display.RGBA));
+    //-display.addMap(new ScalarMap(RealType.getRealType("x"), Display.XAxis));
+    //-display.addMap(new ScalarMap(RealType.getRealType("y"), Display.YAxis));
+    display.addMap(new ScalarMap(RealType.getRealType("x_a"), Display.XAxis));
+    display.addMap(new ScalarMap(RealType.getRealType("y_a"), Display.YAxis));
+    display.addMap(new ScalarMap(RealType.getRealType("val"), Display.RGB));
     
     ScalarMap aniMap = new ScalarMap(RealType.Time, Display.Animation);
     display.addMap(aniMap);
     AnimationControl aniCtrl = (AnimationControl) aniMap.getControl();
-    aniCtrl.setStep(1000);
+    aniCtrl.setStep(500);
     aniCtrl.setOn(true);
     display.addReferences(imgRend, ref);
     
@@ -99,13 +116,13 @@ public class ImageAnimationTest extends JPanel {
     add(display.getComponent());
     
     try {
-      Thread.sleep(1000); 
+      Thread.sleep(1000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    
-    BranchGroup scene = ((DisplayRendererJ3D) imgRend.getDisplayRenderer()).getRoot();
-    Util.printSceneGraph(scene);
+
+    //-BranchGroup scene = ((DisplayRendererJ3D) imgRend.getDisplayRenderer()).getRoot();
+    //-Util.printSceneGraph(scene);
   }
 
   public static void main(String[] args) throws RemoteException, VisADException {
@@ -125,8 +142,10 @@ public class ImageAnimationTest extends JPanel {
     JFrame frame = new JFrame("Image Animation Test");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
-    frame.add(new ImageAnimationTest(num, size));
+    final ImageAnimationTest aniTest = new ImageAnimationTest(num, size);
+    frame.add(aniTest);
     frame.setSize(600, 600);
     frame.setVisible(true);
+
   }
 }
