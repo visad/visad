13c86aa7781e90fede08f8a0ff9a787014a83714
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2009-04-28 18:55:53 +0000

    If the thread manager is only running 1 thread, i.e., sequentially, then run the runnables in the calling thread. This minizes the possbility of side effects due to the threading

--- core/src/visad/trunk/util/ThreadManager.java
+++ core/src/visad/trunk/util/ThreadManager.java
@@ -257,92 +257,86 @@ public class ThreadManager {
    * @throws RemoteException 
    * @throws VisADException 
    */
-  public void runInParallel(boolean doAverage)
-          throws VisADException, RemoteException {
-    myMaxThreads = Math.max(myMaxThreads, 1);
-    myMaxThreads = Math.min(myMaxThreads, MAX_THREADS);
-    int max = Math.min(myMaxThreads, runnables.size());
-    int min = max;
-    ThreadPool pool;
-    try {
-      pool = new ThreadPool("thread util", min, max);
-    } catch (Exception exc) {
-      throw new RuntimeException(exc);
-    }
-    long t1 = System.currentTimeMillis();
-    running = true;
-    int tcnt = 0;
-    final String tlbl = "thread:" + (tcnt++);
-    
-    for (MyRunnable myRunnable : runnables) {
-      runnableStarted();
-      final MyRunnable theRunnable = myRunnable;
-      Runnable runnable = new Runnable() {
-        public void run() {
-          try {
-            //            System.err.println ("start:" + tlbl);
-            theRunnable.run();
-            //            System.err.println ("end:" + tlbl);
-          }
-          catch (Exception exc) {
-            handleException(exc);
-          }
-          finally {
-            runnableStopped();
-          }
+    public void runInParallel(boolean doAverage)
+        throws VisADException, RemoteException {
+        myMaxThreads = Math.max(myMaxThreads, 1);
+        myMaxThreads = Math.min(myMaxThreads, MAX_THREADS);
+        int max = Math.min(myMaxThreads, runnables.size());
+        int min = max;
+        running = true;
+        //If we are not running in parallel then just run in this thread
+        //so we minimize any side effects
+        long t1 = System.currentTimeMillis();
+        if(max<2) {
+            for (MyRunnable myRunnable : runnables) {
+                try {
+                    myRunnable.run();
+                } catch (Exception exc) {
+                    handleException(exc);
+                } 
+                checkErrors();
+            }
+        } else {
+            ThreadPool pool;
+            try {
+                pool = new ThreadPool("thread util", min, max);
+            } catch (Exception exc) {
+                throw new RuntimeException(exc);
+            }
+            for (MyRunnable myRunnable : runnables) {
+                runnableStarted();
+                final MyRunnable theRunnable = myRunnable;
+                Runnable runnable = new Runnable() {
+                        public void run() {
+                            try {
+                                theRunnable.run();
+                            } catch (Exception exc) {
+                                handleException(exc);
+                            } finally {
+                                runnableStopped();
+                            }
+                        }
+                    };
+                pool.queue(runnable);
+            }
+
+            try {
+                pool.waitForTasks();
+                checkErrors();
+            }  finally {
+                try {
+                    pool.stopThreads();
+                } catch (Exception ignoreThis) {}
+            }
+
         }
-      };
-      pool.queue(runnable);
+
+
+        running = false;
+
+        long t2 = System.currentTimeMillis();
+        if(debug) {
+            System.err.println(
+                               name + " time:" + (t2 - t1) + " max threads:" + myMaxThreads);
+        }
+        if (doAverage && false) {
+            Integer[] tuple = times.get(new Integer(myMaxThreads));
+            if (tuple == null) {
+                times.put(new Integer(myMaxThreads), tuple = new Integer[] {0, 0});
+            }
+            tuple[0] = new Integer(tuple[0].intValue() + 1);
+            tuple[1] = new Integer(tuple[1].intValue() + (int)(t2 - t1));
+            System.err.print("   times:");
+            for (Enumeration keys = times.keys(); keys.hasMoreElements(); ) {
+                Integer maxThreads = (Integer)keys.nextElement();
+                Integer[] values = times.get(maxThreads);
+                System.err.print(
+                                 "  #:" + maxThreads + " avg:" +
+                                 (int)(values[1].intValue() / (double)values[0].intValue()));
+            }
+            System.err.println("");
+        }
     }
-
-    try {
-      //      System.err.println ("calling waitForTasks");
-      pool.waitForTasks();
-      //      System.err.println ("after calling waitForTasks");
-      //            System.err.println ("waiting");
-      //            while(numThreadsRunning>0) {
-      //                try {Thread.currentThread().sleep(5);} catch (Throwable exc) {}
-      //                checkErrors();
-      //            }
-      checkErrors();
-    }  finally {
-      try {
-          //        System.err.println ("calling stopThreads");
-        pool.stopThreads();
-        //        System.err.println ("after calling stopThreads");
-      }
-      catch (Exception ignoreThis) {
-      }
-    }
-
-
-    running = false;
-
-    long t2 = System.currentTimeMillis();
-    if(debug)
-        System.err.println(
-                           name + " time:" + (t2 - t1) + " max threads:" + myMaxThreads);
-    if (doAverage && false) {
-      Integer[] tuple = times.get(new Integer(myMaxThreads));
-      if (tuple == null) {
-        times.put(new Integer(myMaxThreads), tuple = new Integer[] {0, 0});
-      }
-      tuple[0] = new Integer(tuple[0].intValue() + 1);
-      tuple[1] = new Integer(tuple[1].intValue() + (int)(t2 - t1));
-      System.err.print("   times:");
-      for (Enumeration keys = times.keys(); keys.hasMoreElements(); ) {
-        Integer maxThreads = (Integer)keys.nextElement();
-        Integer[] values = times.get(maxThreads);
-        System.err.print(
-          "  #:" + maxThreads + " avg:" +
-          (int)(values[1].intValue() / (double)values[0].intValue()));
-      }
-      System.err.println("");
-    }
-
-
-
-  }
 
 
 
