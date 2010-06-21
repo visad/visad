02a267cba437bc5cdee33bdadcb45f0a9609262a
Bruce Flynn <brucef@ssec.wisc.edu>
2010-06-21 04:28:16 +0000

    Remove redundant SVN structure.

--- deps/src/dods/tags/DODS_1_1_1/clients/importwizard/DataURLProcessor.java
+++
@@ -1,8 +0,0 @@
-package dods.clients.importwizard;
-
-import javax.swing.*;
-
-public abstract class DataURLProcessor extends JPanel {
-    public abstract DodsURL getURL();
-    public abstract void updateCE();
-}
