20ff34eaefb5c872d8c8543608981873d156e78b
Tom Rink <rink@ssec.wisc.edu>
2013-08-04 21:41:24 -0500

    Add display level enabling/disabling of the depth buffer (default remains: enabled).  This allows 3D displays in 2D mode to have last DataReference added rendered on top w/o required ScalarMaps to Display.ZAxis.  Useful in displays with line, point and non-filled geometry when transparency is not required.  (2) Add some rendering attribute capabilities for more flexibility.

--- core/src/visad/java3d/GraphicsModeControlJ3D.java
+++ core/src/visad/java3d/GraphicsModeControlJ3D.java
@@ -96,6 +96,9 @@ public class GraphicsModeControlJ3D extends GraphicsModeControl {
   /** for merging geometries */
   private boolean mergeGeometries = false;
 
+  /** for depth buffer enabling */
+  private boolean depthBufferEnable = true;
+
   /**
    * Construct a GraphicsModeControlJ3D associated with the input display
    *
@@ -621,6 +624,20 @@ public class GraphicsModeControlJ3D extends GraphicsModeControl {
     return polygonOffsetFactor;
   }
 
+  public void setDepthBufferEnable(boolean enable) 
+    throws VisADException, RemoteException {
+    this.depthBufferEnable = enable;
+    changeControl(true);
+    getDisplay().reDisplayAll();
+  }
+
+  public void setDepthBufferEnable(boolean enable, boolean noChange) {
+    this.depthBufferEnable = enable;
+  }
+
+  public boolean getDepthBufferEnable() {
+    return depthBufferEnable;
+  }
 
   /**
    * See whether missing values are rendered as transparent or not.
@@ -770,6 +787,7 @@ public class GraphicsModeControlJ3D extends GraphicsModeControl {
     mode.texture3DMode = texture3DMode;
     mode.cacheAppearances = cacheAppearances;
     mode.mergeGeometries = mergeGeometries;
+    mode.depthBufferEnable = depthBufferEnable;
     return mode;
   }
 
