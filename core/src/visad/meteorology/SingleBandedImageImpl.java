3c7c15fcacfefaec06d84b37cf53775b0bc21a88
Curtis Rueden <ctrueden@wisc.edu>
2015-02-11 06:52:56 -0600

    Happy belated New Year 2015!

--- core/src/visad/meteorology/SingleBandedImageImpl.java
+++ core/src/visad/meteorology/SingleBandedImageImpl.java
@@ -2,7 +2,7 @@
 
 /*
 VisAD system for interactive analysis and visualization of numerical
-data.  Copyright (C) 1996 - 2014 Bill Hibbard, Curtis Rueden, Tom
+data.  Copyright (C) 1996 - 2015 Bill Hibbard, Curtis Rueden, Tom
 Rink, Dave Glowacki, Steve Emmerson, Tom Whittaker, Don Murray, and
 Tommy Jasmin.
 
