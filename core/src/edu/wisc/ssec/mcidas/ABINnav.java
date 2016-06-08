4268a1f396a1cbda81adcd08eb13569ea911bd60
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2016-06-08 15:38:36 -0500

    ABINnav javadocs and cleanup.

--- core/src/edu/wisc/ssec/mcidas/ABINnav.java
+++ core/src/edu/wisc/ssec/mcidas/ABINnav.java
@@ -1,10 +1,10 @@
 //
-// ABISnav.java
+// ABINnav.java
 //
 
 /*
 This source file is part of the edu.wisc.ssec.mcidas package and is
-Copyright (C) 1998 - 2015 by Tom Whittaker, Tommy Jasmin, Tom Rink,
+Copyright (C) 1998 - 2016 by Tom Whittaker, Tommy Jasmin, Tom Rink,
 Don Murray, James Kelly, Bill Hibbard, Dave Glowacki, Curtis Rueden
 and others.
 
@@ -27,47 +27,93 @@ MA 02111-1307, USA
 package edu.wisc.ssec.mcidas;
 
 import static java.lang.Math.PI;
-import static java.lang.Math.atan;
-import static java.lang.Math.atan2;
-import static java.lang.Math.tan;
 import static java.lang.Math.asin;
-import static java.lang.Math.sin;
+import static java.lang.Math.atan;
 import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
-import static java.lang.Math.pow;
+import static java.lang.Math.tan;
 
+/**
+ * ABINnav is used to provide {@literal "navigation"} for ABIN image data.
+ *
+ * This code is essentially a direct port of {@code nvxabin.dlm} from McIDAS-X.
+ * Note: the variable naming convention has been retained from the original
+ * McIDAS-X source code.
+ */
 public class ABINnav extends AREAnav {
 
-    private static final double rdpdg = 1.745329252E-2;
-
-    private static final double bsq = 40410330.18;
-
-    private static final double asq = 40683833.48;
-
-    private static final double ab = 40546851.22;
-
+    /** Radius of satellite orbit (kilometers). */
     private static final double dh = 42164.16000000000349245965480804443359375;
+
+    /** Convert degrees to radians. */
     private static final double deg_to_rad = PI / 180.0;
-    private static final double dreq2 = 6378.136999999999716237653046846389770508 * 6378.136999999999716237653046846389770508;
-    private static final double drpo = 6356.752300000000104773789644241333007812;
+
+    /** Radius of the Earth at the equator (kilometers). */
+    private static final double r_eq =
+        6378.136999999999716237653046846389770508;
+
+    /** Radius of the Earth at the equator squared (kilometers). */
+    // note: dreq2 and r_eq2 have been kept around to conform to McIDAS-X code
+    private static final double dreq2 = r_eq * r_eq;
+
+    /** Radius of the Earth at the poles (kilometers). */
+    private static final double drpo =
+        6356.752300000000104773789644241333007812;
+
+    /** Radius of the Earth at the poles squared (kilometers). */
     private static final double drpo2 = drpo * drpo;
 
-    // radius of the Earth at the equator squared
-    private static final double r_eq2 = 6378.136999999999716237653046846389770508 * 6378.136999999999716237653046846389770508;
-    private static final double d = 42164.16000000000349245965480804443359375 * 42164.16000000000349245965480804443359375 - r_eq2;
+    /** Radius of the Earth at the equator squared (kilometers). */
+    // note: dreq2 and r_eq2 have been kept around to conform to McIDAS-X code
+    private static final double r_eq2 = dreq2;
+
+    /**
+     * Pre-calculated constant (value is dh^2 - r_eq^2).
+     *
+     * @see #dh
+     * @see #r_eq
+     */
+    private static final double d = dh * dh - r_eq2;
+
+    private static final double FP =
+        1.006802999999999892466462370066437870264;
 
     private boolean isEastPositive = true;
-    private double loff;
-    private double coff;
-    private double lfac;
-    private double cfac;
-    private double plon;
-    private double plaxh;
-    private double bres;
 
     private int itype = 1;
 
+    /** Line offset. */
+    private double loff;
+
+    /** Column offset. */
+    private double coff;
+
+    /** Line factor. */
+    private double lfac;
+
+    /** Column factor. */
+    private double cfac;
+
+    /** Subpoint. */
+    private double plon;
+
+    /** Base resolution. */
+    private double bres;
+
+    /**
+     * Initializes the ABIN navigation code with the given set of navigation
+     * parameters.
+     *
+     * @param navblock Navigation parameters from image file.
+     *
+     * @throws IllegalArgumentException if {@code navblock} is not ABIN.
+     */
     public ABINnav(int[] navblock) {
+        if (navblock[0] != ABIN) {
+            throw new IllegalArgumentException("Invalid navigation type: " +
+                                               navblock[0]);
+        }
         loff = navblock[1] / 100000000.0;
         coff = navblock[2] / 100000000.0;
         lfac = navblock[3] / 100000000.0;
@@ -76,7 +122,21 @@ public class ABINnav extends AREAnav {
         bres = navblock[6];
     }
 
+    /**
+     * Convert satellite lines/elements to latitude/longitude coordinates.
+     *
+     * @param linele Array of line/element pairs.
+     *               Where {@code linele[indexLine]} are {@literal "lines"}
+     *               and {@code linele[indexEle]} are {@literal "elements"}.
+     *               These coordinates must be {@literal "file"} rather than
+     *               {@literal "image"} coordinates.
+     *
+     * @return Array of latitude/longitude pairs. {@code latlon[indexLat]} are
+     *         latitudes and {@code latlon[indexLon]} are longitudes.
+     */
     public double[][] toLatLon(double[][] linele) {
+        final double sub_lon_radians = plon * (PI / 180.0);
+
         double xlin;
         double xele;
         double lamda_goes;
@@ -96,11 +156,11 @@ public class ABINnav extends AREAnav {
         double s2;
         double s3;
         double sxy;
-        double sub_lon_radians = plon * (PI / 180.0);
 
         int length = linele[indexLine].length;
         double[][] latLons = new double[2][length];
         double[][] imageLineElems = areaCoordToImageCoord(linele);
+
         for (int point = 0; point < length; point++) {
             double rlin = imageLineElems[indexLine][point];
             double rele = imageLineElems[indexEle][point];
@@ -130,15 +190,13 @@ public class ABINnav extends AREAnav {
             double xlat;
             double xlon;
 
-            // sub_lon_radians = plon * (PI / 180.0);
-
             // adjust using Base RESolution
             xlin = (rlin + bres - 1) / bres;
             xele = (rele + bres - 1) / bres;
 
             // Intermediate coordinates (coordinates will be radians)
-            theta_goes = (xlin * lfac + loff);
-            lamda_goes = (xele * cfac + coff);
+            theta_goes = xlin * lfac + loff;
+            lamda_goes = xele * cfac + coff;
 
             // convert GOES to GEOS
             theta_geos = asin(sin(theta_goes) * cos(lamda_goes));
@@ -150,26 +208,26 @@ public class ABINnav extends AREAnav {
             sinx = sin(lamda_geos);
             siny = sin(theta_geos);
 
-            c1 = 42164.16000000000349245965480804443359375 * cosx * cosy * 42164.16000000000349245965480804443359375 * cosx * cosy;
-            c2 = (cosy * cosy + 1.006802999999999892466462370066437870264 * siny * siny) * d;
-            //
+            c1 = dh * cosx * cosy * dh * cosx * cosy;
+            c2 = (cosy * cosy + FP * siny * siny) * d;
+
             sdd = c1 - c2;
             if ((sdd < 0.0))  {
                 xlat = Double.NaN;
                 xlon = Double.NaN;
             } else {
                 sd = sqrt(sdd);
-                //
-                sn = (42164.16000000000349245965480804443359375 * cosx * cosy - sd) / (cosy * cosy + 1.006802999999999892466462370066437870264 * siny * siny);
-                //
-                s1 = 42164.16000000000349245965480804443359375 - sn * cosx * cosy;
+
+                sn = (dh * cosx * cosy - sd) / (cosy * cosy + FP * siny * siny);
+
+                s1 = dh - sn * cosx * cosy;
                 s2 = sn * sinx * cosy;
                 s3 = -(sn * siny);
-                //
+
                 sxy = sqrt(s1 * s1 + (s2 * s2));
                 xlon = atan(s2 / s1) + sub_lon_radians;
-                //
-                xlat = atan(-(1.006802999999999892466462370066437870264 * s3 / sxy));
+
+                xlat = atan(-(FP * s3 / sxy));
 
                 // convert radians to degrees
                 xlon = xlon * (180.0 / PI);
@@ -191,13 +249,25 @@ public class ABINnav extends AREAnav {
         return latLons;
     }
 
+    /**
+     * Convert latitudes/longitudes to satellite lines/elements.
+     *
+     * @param latlon Array of latitude/longitude pairs.
+     *               Where {@code latlon[indexLat]} are latitudes and
+     *               {@code latlon[indexLon]} are longitudes.
+     *
+     * @return Array of line/element pairs. {@code linele[indexLine]} are lines
+     *         and {@code linele[indexEle]} are elements. These coordinates are
+     *         {@literal "file"} rather than {@literal "image"} coordinates.
+     */
     public double[][] toLinEle(double[][] latlon) {
+        final double d_geographic_ssl = plon * deg_to_rad;
+
         double rlin;
         double rele;
         double d_geographic_lat;
         double d_geocentric_lat;
         double d_geographic_lon;
-        double d_geographic_ssl;
         double r_earth;
         double r_1;
         double r_2;
@@ -207,6 +277,7 @@ public class ABINnav extends AREAnav {
 
         int length = latlon[indexLat].length;
         double[][] lineEles = new double[2][length];
+
         for (int point = 0; point < length; point++) {
             double rlat = latlon[indexLat][point];
             double rlon = latlon[indexLon][point];
@@ -220,7 +291,6 @@ public class ABINnav extends AREAnav {
             d_geographic_lat = 0.0;
             d_geocentric_lat = 0.0;
             d_geographic_lon = 0.0;
-            d_geographic_ssl = 0.0;
             r_earth = 0.0;
             r_1 = 0.0;
             r_2 = 0.0;
@@ -234,7 +304,6 @@ public class ABINnav extends AREAnav {
             // Earth (Geographic) Coordinates are converted to Radians
             d_geographic_lat = rlat * deg_to_rad;
             d_geographic_lon = rlon * deg_to_rad;
-            d_geographic_ssl = plon * deg_to_rad;
 
             d_geocentric_lat = atan(drpo2 / dreq2 * tan(d_geographic_lat));
 
@@ -252,17 +321,17 @@ public class ABINnav extends AREAnav {
             } else {
                 lamda = asin(-(r_2 / sqrt(r_1 * r_1 + r_2 * r_2 + r_3 * r_3)));
                 theta = atan(r_3 / r_1);
+
                 // image line and element
                 rlin = (theta - loff) / lfac;
                 rele = (lamda - coff) / cfac;
+
                 // Adjust using Base RESolution
                 xlin = rlin * bres - (bres - 1);
                 xele = rele * bres - (bres - 1);
             }
             // end of ll_to_img
 
-//            lineEles[indexLine][point] = lineEle[0];
-//            lineEles[indexEle][point] = lineEle[1];
             lineEles[indexLine][point] = xlin;
             lineEles[indexEle][point] = xele;
         }
