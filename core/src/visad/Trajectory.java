690e20ccc010da06baea2e3f5dce3ee3916a4509
rink <rink@moonunit.ssec.wisc.edu>
2016-06-21 12:55:57 -0500

    minor

--- core/src/visad/Trajectory.java
+++ core/src/visad/Trajectory.java
@@ -49,13 +49,12 @@ public class Trajectory {
 
   int npairs = 0;
   int[] indexes = new int[60];
-  float lastx = Float.NaN;
-  float lasty = Float.NaN;
-  float lastz = Float.NaN;
+  
   float[] lastPtD = null;
   float[] lastPtC = null;
   float[] lastPtDD = null;
   float[] lastPtCC = null;
+  
   float[][] circleXYZ;
   float[][] last_circleXYZ;  
   
@@ -186,9 +185,6 @@ public class Trajectory {
      trajMan.addPair(startPt, stopPt, startColor, stopColor);    
 
      npairs++;
-     lastx = stopPt[0];
-     lasty = stopPt[1];
-     lastz = stopPt[2];
 
      int clrDim = startColor.length;
 
@@ -200,119 +196,119 @@ public class Trajectory {
      }     
   } 
   
-     public VisADGeometryArray makeCylinderStrip(float[] T, float[] S, float[] pt0, float[] pt1, byte[][] clr0, byte[][] clr1, float size,
-                 int npts, float[] coords, byte[] colors, float[] normls, int[] vertCnt) {
-        VisADTriangleStripArray array = new VisADTriangleStripArray();
-        
-        int clrDim = clr0.length;
-        
-         if (circle == null) {
-           circle = new float[2][npts];
-           float intrvl = (float) (2*Math.PI)/(npts-1);
-           for (int i=0; i<npts; i++) {
-              circle[0][i] = (float) Math.cos(intrvl*i);  // s
-              circle[1][i] = (float) Math.sin(intrvl*i);  // t
-           }
+  public VisADGeometryArray makeCylinderStrip(float[] T, float[] S, float[] pt0, float[] pt1, byte[][] clr0, byte[][] clr1, float size,
+              int npts, float[] coords, byte[] colors, float[] normls, int[] vertCnt) {
+     VisADTriangleStripArray array = new VisADTriangleStripArray();
+
+     int clrDim = clr0.length;
+
+      if (circle == null) {
+        circle = new float[2][npts];
+        float intrvl = (float) (2*Math.PI)/(npts-1);
+        for (int i=0; i<npts; i++) {
+           circle[0][i] = (float) Math.cos(intrvl*i);  // s
+           circle[1][i] = (float) Math.sin(intrvl*i);  // t
         }
-        
-        int vcnt = vertCnt[0];
-        int idx = 3*vcnt;
-        int cidx = clrDim*vcnt;
-        
-        if (circleXYZ == null) {
-           circleXYZ = new float[3][npts];
-        }
+     }
+
+     int vcnt = vertCnt[0];
+     int idx = 3*vcnt;
+     int cidx = clrDim*vcnt;
+
+     if (circleXYZ == null) {
+        circleXYZ = new float[3][npts];
+     }
+     for (int k=0; k<npts; k++) {
+        float s = size*circle[0][k];
+        float t = size*circle[1][k];
+        circleXYZ[0][k] = pt1[0] + s*S[0] + t*T[0];
+        circleXYZ[1][k] = pt1[1] + s*S[1] + t*T[1];
+        circleXYZ[2][k] = pt1[2] + s*S[2] + t*T[2];
+     }
+     if (last_circleXYZ == null) { // first time
+        float[][] ptsXYZ = new float[3][npts];
         for (int k=0; k<npts; k++) {
            float s = size*circle[0][k];
            float t = size*circle[1][k];
-           circleXYZ[0][k] = pt1[0] + s*S[0] + t*T[0];
-           circleXYZ[1][k] = pt1[1] + s*S[1] + t*T[1];
-           circleXYZ[2][k] = pt1[2] + s*S[2] + t*T[2];
+           ptsXYZ[0][k] = pt0[0] + s*S[0] + t*T[0];
+           ptsXYZ[1][k] = pt0[1] + s*S[1] + t*T[1];
+           ptsXYZ[2][k] = pt0[2] + s*S[2] + t*T[2];
         }
-        if (last_circleXYZ == null) { // first time
-           float[][] ptsXYZ = new float[3][npts];
-           for (int k=0; k<npts; k++) {
-              float s = size*circle[0][k];
-              float t = size*circle[1][k];
-              ptsXYZ[0][k] = pt0[0] + s*S[0] + t*T[0];
-              ptsXYZ[1][k] = pt0[1] + s*S[1] + t*T[1];
-              ptsXYZ[2][k] = pt0[2] + s*S[2] + t*T[2];
-           }
-           last_circleXYZ = new float[3][npts];
-           System.arraycopy(ptsXYZ[0], 0, last_circleXYZ[0], 0, npts);
-           System.arraycopy(ptsXYZ[1], 0, last_circleXYZ[1], 0, npts);
-           System.arraycopy(ptsXYZ[2], 0, last_circleXYZ[2], 0, npts);
-        }
-        
-       for (int k=0; k<npts; k++) {
-          float x = last_circleXYZ[0][k];
-          float y = last_circleXYZ[1][k];
-          float z = last_circleXYZ[2][k];
-          
-          float delx = x - pt0[0];
-          float dely = y - pt0[1];
-          float delz = z - pt0[2];
-          
-          float mag = (float) Math.sqrt(delx*delx+dely*dely+delz*delz);
+        last_circleXYZ = new float[3][npts];
+        System.arraycopy(ptsXYZ[0], 0, last_circleXYZ[0], 0, npts);
+        System.arraycopy(ptsXYZ[1], 0, last_circleXYZ[1], 0, npts);
+        System.arraycopy(ptsXYZ[2], 0, last_circleXYZ[2], 0, npts);
+     }
 
-          normls[idx] = delx/mag;
-          coords[idx++] = x;
-          normls[idx] = dely/mag;
-          coords[idx++] = y;
-          normls[idx] = delz/mag;
-          coords[idx++] = z;
+    for (int k=0; k<npts; k++) {
+       float x = last_circleXYZ[0][k];
+       float y = last_circleXYZ[1][k];
+       float z = last_circleXYZ[2][k];
 
-          if (clrDim == 3) {
-             colors[cidx++] = clr0[0][0];
-             colors[cidx++] = clr0[1][0];
-             colors[cidx++] = clr0[2][0];
-          }
-          else { // must be four
-             colors[cidx++] = clr0[0][0];
-             colors[cidx++] = clr0[1][0];
-             colors[cidx++] = clr0[2][0];
-             colors[cidx++] = clr0[3][0];
-          }
-          vcnt++;
+       float delx = x - pt0[0];
+       float dely = y - pt0[1];
+       float delz = z - pt0[2];
 
-          x = circleXYZ[0][k];
-          y = circleXYZ[1][k];
-          z = circleXYZ[2][k];
-          
-          delx = x - pt1[0];
-          dely = y - pt1[1];
-          delz = z - pt1[2];
-          
-          mag = (float) Math.sqrt(delx*delx+dely*dely+delz*delz);
-          
-          normls[idx] = delx/mag;              
-          coords[idx++] = x;
-          normls[idx] = dely/mag;              
-          coords[idx++] = y;
-          normls[idx] = delz/mag;             
-          coords[idx++] = z;
+       float mag = (float) Math.sqrt(delx*delx+dely*dely+delz*delz);
 
-          if (clrDim == 3) {
-             colors[cidx++] = clr1[0][0];
-             colors[cidx++] = clr1[1][0];
-             colors[cidx++] = clr1[2][0];
-          }
-          else { // must be four
-             colors[cidx++] = clr1[0][0];
-             colors[cidx++] = clr1[1][0];
-             colors[cidx++] = clr1[2][0];
-             colors[cidx++] = clr1[3][0];
-          }
-          vcnt++;
+       normls[idx] = delx/mag;
+       coords[idx++] = x;
+       normls[idx] = dely/mag;
+       coords[idx++] = y;
+       normls[idx] = delz/mag;
+       coords[idx++] = z;
+
+       if (clrDim == 3) {
+          colors[cidx++] = clr0[0][0];
+          colors[cidx++] = clr0[1][0];
+          colors[cidx++] = clr0[2][0];
        }
+       else { // must be four
+          colors[cidx++] = clr0[0][0];
+          colors[cidx++] = clr0[1][0];
+          colors[cidx++] = clr0[2][0];
+          colors[cidx++] = clr0[3][0];
+       }
+       vcnt++;
 
-           
-        System.arraycopy(circleXYZ[0], 0, last_circleXYZ[0], 0, npts);
-        System.arraycopy(circleXYZ[1], 0, last_circleXYZ[1], 0, npts);
-        System.arraycopy(circleXYZ[2], 0, last_circleXYZ[2], 0, npts);
-     
-        vertCnt[0] = vcnt;
-        return array;
-     }  
+       x = circleXYZ[0][k];
+       y = circleXYZ[1][k];
+       z = circleXYZ[2][k];
+
+       delx = x - pt1[0];
+       dely = y - pt1[1];
+       delz = z - pt1[2];
+
+       mag = (float) Math.sqrt(delx*delx+dely*dely+delz*delz);
+
+       normls[idx] = delx/mag;              
+       coords[idx++] = x;
+       normls[idx] = dely/mag;              
+       coords[idx++] = y;
+       normls[idx] = delz/mag;             
+       coords[idx++] = z;
+
+       if (clrDim == 3) {
+          colors[cidx++] = clr1[0][0];
+          colors[cidx++] = clr1[1][0];
+          colors[cidx++] = clr1[2][0];
+       }
+       else { // must be four
+          colors[cidx++] = clr1[0][0];
+          colors[cidx++] = clr1[1][0];
+          colors[cidx++] = clr1[2][0];
+          colors[cidx++] = clr1[3][0];
+       }
+       vcnt++;
+    }
+
+
+     System.arraycopy(circleXYZ[0], 0, last_circleXYZ[0], 0, npts);
+     System.arraycopy(circleXYZ[1], 0, last_circleXYZ[1], 0, npts);
+     System.arraycopy(circleXYZ[2], 0, last_circleXYZ[2], 0, npts);
+
+     vertCnt[0] = vcnt;
+     return array;
+  }  
    
 }
